7ff1632ef59957984cce9e4492b97c690a12286a
id774 <idnanashi@gmail.com>
2010-10-19 11:01:16 +0900

    #11 FileIO in java

--- java/javaiosample/JavaIOSampleMain.java
+++ java/javaiosample/JavaIOSampleMain.java
@@ -1,11 +1,12 @@
 package javaiosample;
 
 public class JavaIOSampleMain {
-		public static void main(String[] args) {
-			FileHandler f = new FileHandler();
-			f.createNewFile("newfile.txt");
-			f.createNewFolder("newfolder");
-			f.createNewFolderTree("newfoldertree");
-			f.fileExistCheck("newfile.txt");
+	public static void main(String[] args) {
+		FileHandler f = new FileHandler();
+		f.createNewFile("newfile.txt");
+		f.createNewFolder("newfolder");
+		f.createNewFolderTree("newfoldertree");
+		f.fileExistCheck("newfile.txt");
+		f.printCurrentDir();
 	}
 }
