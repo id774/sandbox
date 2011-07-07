ffac9ab6e61a48f8d3ba64ebcb0d928a957dc3c4
id774 <idnanashi@gmail.com>
2011-07-08 00:41:08 +0900

    #25 web.py template example

--- python/web.py/sample/code.py
+++ python/web.py/sample/code.py
@@ -3,14 +3,16 @@
 import web
 
 class index:
-    def GET(self):
-        return "Hello, world!"
+    def GET(self, name):
+        i = web.input(name=None)
+        return render.index(i.name)
 
 urls = (
-        '/', 'index'
+        '/(.*)', 'index'
         )
 
 app = web.application(urls, globals())
+render = web.template.render('templates/')
 
 if __name__ == "__main__":
     app.run()
