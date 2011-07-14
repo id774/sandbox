c3236f11919e876429805f1349cf9b6299873779
id774 <idnanashi@gmail.com>
2011-07-14 10:34:01 +0900

    #26 web.py on mod_wsgi sample

---
+++ python/web.py/sample_with_mod_wsgi/code.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# Add following to /etc/apache2/conf.d/mod_wsgi.conf
+#WSGIScriptAlias /sample /var/www/webpy/sample/code.py
+
+import web
+
+class index:
+    def GET(self, name):
+        i = web.input(name=None)
+        return render.index(i.name)
+
+urls = (
+        '/(.*)', 'index'
+        )
+
+#application = web.application(urls, globals())
+application = web.application(urls, globals()).wsgifunc()
+
+render = web.template.render('/var/www/webpy/sample/templates/')
+
+if __name__ == "__main__":
+    application.run()
+
