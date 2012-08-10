b76cd9da06867407a938c532062babd8fef2a9d3
id774 <idnanashi@gmail.com>
2012-08-10 10:14:44 +0900

    98 First class object in ruby

---
+++ ruby/deriv.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+def deriv(f, dx)
+  ->(x){ f[x + dx] - f[x] }
+end
+ 
+sin = ->(x){ Math.sin(x) }
+ 
+cos = deriv(sin, 0.00000001)
+ 
+puts sin[Math::PI / 2]
+puts cos[Math::PI / 2]
