4af065110c7c7503bbccc658fb2b61152f06264a
id774 <idnanashi@gmail.com>
2012-08-24 22:40:16 +0900

    118 Print score of intermediate values of the tree

--- python/decision_tree/treepredict.py
+++ python/decision_tree/treepredict.py
@@ -4,10 +4,22 @@ from PIL import Image,ImageDraw,ImageFont
 import sys
 import re, pprint
 
+# コーパスの読み込み
 #mydata=[line.split('\t') for line in file('decision_tree_example.txt')]
 #mydata=[line.split(' ') for line in file('data_format_sample2.txt')]
 fin = file(sys.argv[1])
 mydata=[line.rstrip().split(' ') for line in fin]
+
+def print_score(set1,set2,gain,col,value,column_values):
+    print '項目/値: ',col,value
+    print '取りうる値: ',column_values
+    print '集団1: ',pp(set1)
+    print '集団1のジニ不純度: ',giniimpurity(set1)
+    print '集団1のエントロピー: ',entropy(set1)
+    print '集団2: ',pp(set2)
+    print '集団2のジニ不純度: ',giniimpurity(set2)
+    print '集団2のエントロピー: ',entropy(set2)
+    print '情報ゲイン: ',gain
 
 def pp(obj):
     pp=pprint.PrettyPrinter(indent=4,width=160)
@@ -86,70 +98,6 @@ def getdepth(tree):
     if tree.tb==None and tree.fb==None: return 0
     return max(getdepth(tree.tb),getdepth(tree.fb))+1
 
-def classify(observation,tree):
-    if tree.results!=None:
-        return tree.results
-    else:
-        v=observation[tree.col]
-        branch=None
-    if isinstance(v,int) or isinstance(v,float):
-        if v>=tree.value: branch=tree.tb
-        else: branch=tree.fb
-    else:
-        if v==tree.value: branch=tree.tb
-        else: branch=tree.fb
-    return classify(observation,branch)
-
-def prune(tree,mingain):
-    # 葉でない枝に刈り込み
-    if tree.tb.results==None:
-        prune(tree.tb,mingain)
-    if tree.fb.results==None:
-        prune(tree.fb,mingain)
-    # 両方を統合するべきか判定
-    if tree.tb.results!=None and tree.fb.results!=None:
-        tb,fb=[],[]
-        for v,c in tree.tb.results.items():
-            tb+=[[v]]*c
-        for v,c in tree.fb.results.items():
-            fb+=[[v]]*c
-        # エントロピーの現象を調べる
-        delta=entropy(tb+fb)-(entropy(tb)+entropy(fb)/2)
-        if delta<mingain:
-            tree.tb,tree.fb=None,None
-            tree.results=uniquecounts(tb+fb)
-
-def mdclassify(observation,tree):
-    if tree.results!=None:
-        return tree.results
-    else:
-        v=observation[tree.col]
-        if v==None:
-            tr,fr=mdclassify(observation,tree.tb),mdclassify(observation,tree.fb)
-            tcount=sum(tr.values())
-            fcount=sum(fr.values())
-            tw=float(tcount)/(tcount+fcount)
-            fw=float(fcount)/(tcount+fcount)
-            result={}
-            for k,v in tr.items(): result[k]=v*tw
-            for k,v in fr.items(): result[k]=v*fw
-            return result
-        else:
-            if isinstance(v,int) or isinstance(v,float):
-                if v>=tree.value: branch=tree.tb
-                else: branch=tree.fb
-            else:
-                if v==tree.value: branch=tree.tb
-                else: branch=tree.fb
-            return mdclassify(observation,branch)
-
-def variance(rows):
-    if len(rows)==0: return 0
-    data=[float(row[len(row)-1]) for row in rows]
-    mean=sum(data)/len(data)
-    variance=sum([(d-mean)**2 for d in data])/len(data)
-    return variance
-
 def buildtree(rows,scoref=entropy):
     if len(rows)==0: return decisionnode()
     current_score=scoref(rows)
@@ -169,7 +117,8 @@ def buildtree(rows,scoref=entropy):
             # ゲイン
             p=float(len(set1))/len(rows)
             gain=current_score-p*scoref(set1)-(1-p)*scoref(set2)
-            # print col,value,gain
+            # 決定木中間地点のスコアを表示
+            print_score(set1,set2,gain,col,value,column_values)
             if gain>best_gain and len(set1)>0 and len(set2)>0:
                 best_gain=gain
                 best_criteria=(col,value)
@@ -230,32 +179,71 @@ def drawnode(draw,tree,x,y):
         utxt=unicode(txt,'utf-8')
         draw.text((x-20,y),utxt,font=font,fill='#000000')
 
+def prune(tree,mingain):
+    # 葉でない枝に刈り込み
+    if tree.tb.results==None:
+        prune(tree.tb,mingain)
+    if tree.fb.results==None:
+        prune(tree.fb,mingain)
+    # 両方を統合するべきか判定
+    if tree.tb.results!=None and tree.fb.results!=None:
+        tb,fb=[],[]
+        for v,c in tree.tb.results.items():
+            tb+=[[v]]*c
+        for v,c in tree.fb.results.items():
+            fb+=[[v]]*c
+        # エントロピーの現象を調べる
+        delta=entropy(tb+fb)-(entropy(tb)+entropy(fb)/2)
+        if delta<mingain:
+            tree.tb,tree.fb=None,None
+            tree.results=uniquecounts(tb+fb)
+
+def variance(rows):
+    if len(rows)==0: return 0
+    data=[float(row[len(row)-1]) for row in rows]
+    mean=sum(data)/len(data)
+    variance=sum([(d-mean)**2 for d in data])/len(data)
+    return variance
+
+def classify(observation,tree):
+    if tree.results!=None:
+        return tree.results
+    else:
+        v=observation[tree.col]
+        branch=None
+    if isinstance(v,int) or isinstance(v,float):
+        if v>=tree.value: branch=tree.tb
+        else: branch=tree.fb
+    else:
+        if v==tree.value: branch=tree.tb
+        else: branch=tree.fb
+    return classify(observation,branch)
+
+def mdclassify(observation,tree):
+    if tree.results!=None:
+        return tree.results
+    else:
+        v=observation[tree.col]
+        if v==None:
+            tr,fr=mdclassify(observation,tree.tb),mdclassify(observation,tree.fb)
+            tcount=sum(tr.values())
+            fcount=sum(fr.values())
+            tw=float(tcount)/(tcount+fcount)
+            fw=float(fcount)/(tcount+fcount)
+            result={}
+            for k,v in tr.items(): result[k]=v*tw
+            for k,v in fr.items(): result[k]=v*fw
+            return result
+        else:
+            if isinstance(v,int) or isinstance(v,float):
+                if v>=tree.value: branch=tree.tb
+                else: branch=tree.fb
+            else:
+                if v==tree.value: branch=tree.tb
+                else: branch=tree.fb
+            return mdclassify(observation,branch)
+
 def main():
-    set1,set2=divideset(mydata,2,'yes')
-
-    print 'ジニ不純度'
-    print '無作為に置いた要素が間違ったカテゴリーに入る確率'
-    print '可能な帰結が 4 種類ありすべて等しく起きるなら誤差率は 0.75'
-    print giniimpurity(mydata)
-
-    print 'エントロピー'
-    print 'p(i) = 頻度(帰結) = 度数(帰結) / 度数(行)'
-    print 'エントロピー = すべての帰結の p(i) x log(p(i)) の合計'
-    print entropy(mydata)
-
-    print '集合 1 の個数'
-    print len(set1)
-    print pp(set1)
-    print '集合 2 の個数'
-    print len(set2)
-    print pp(set2)
-    print '集合 1 の値'
-    print giniimpurity(set1)
-    print entropy(set1)
-    print '集合 2 の値'
-    print giniimpurity(set2)
-    print entropy(set2)
-
     print '決定木の生成'
     tree=buildtree(mydata)
     #prune(tree,0.1)
