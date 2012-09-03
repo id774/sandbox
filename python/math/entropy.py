f4a0e92cb807a78560ed524094fbf4c660b500e2
id774 <idnanashi@gmail.com>
2012-09-03 10:50:53 +0900

    127 Entropy in python

---
+++ python/math/entropy.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# - * - coding: utf-8 - * -
+
+import nltk
+import math
+
+"""
+>>> import entropy
+>>> entropy.entropy(['male', 'male', 'female', 'male'])
+0.811278124459
+"""
+
+def entropy(labels):
+    freqdist = nltk.FreqDist(labels)
+    probs = [freqdist.freq(l) for l in nltk.FreqDist(labels)]
+    return -sum([p * math.log(p,2) for p in probs])
+
+def main():
+    print entropy(['male', 'male', 'female', 'male'])
+
+if __name__=='__main__':
+    import doctest
+    doctest.testmod()
+    main()
+
---
+++ python/math/test_entropy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# - * - coding: utf-8 - * -
+
+from nose.tools import *
+from entropy import *
+
+def test_entropy():
+    i=(['male', 'male', 'female', 'male'])
+    e=0.81127812445913283
+    eq_(e,entropy(i))
+
