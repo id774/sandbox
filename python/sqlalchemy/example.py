0683df6353bd7c8ee8606a2c3d9699c9ec6a560f
id774 <idnanashi@gmail.com>
2012-09-06 17:19:30 +0900

    130 SQLAlchemy select from sqlite example

---
+++ python/sqlalchemy/example.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import sqlalchemy
+
+class Status(object):
+    pass
+
+def create_engine():
+    config = {"sqlalchemy.url": "sqlite:///test.db"}
+    engine = sqlalchemy.engine_from_config(config)
+
+    from sqlalchemy.orm import scoped_session, sessionmaker
+    db_session = scoped_session(sessionmaker(autoflush=True, transactional=True, bind=engine))
+    return engine
+
+def statuses():
+    from sqlalchemy import MetaData
+    from sqlalchemy import Column, Table, types
+    from sqlalchemy.orm import mapper, relation
+    metadata = MetaData()
+    status = Table("statuses", metadata,
+                   Column("id", types.Integer, primary_key=True),
+                   Column("screen_name", types.Unicode, nullable=False),
+                   Column("text", types.Unicode),
+                   Column("created_at", types.Date),
+                   Column("protected", types.Boolean),
+                   Column("in_reply_to_status_id", types.Integer),
+                   Column("in_reply_to_user_id", types.Integer),
+                   Column("in_reply_to_screen_name", types.Unicode),
+                   Column("statuses_count", types.Integer),
+                   Column("friends_count", types.Integer),
+                   Column("followers_count", types.Integer),
+                   Column("source", types.Unicode))
+    mapper(Status, status)
+    return status
+
+def run():
+    engine = create_engine()
+    status = statuses()
+
+    stmt = status.select()
+    result = engine.execute(stmt)
+    print result
+    for row in result:
+        print row.screen_name,row.text
+
+if __name__ == "__main__":
+    run()
