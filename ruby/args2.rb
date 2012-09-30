7f64d3c1d4537df01d0ce03ce371f729ed1e778e
id774 <idnanashi@gmail.com>
2012-09-30 14:02:49 +0900

    139 Variable args example

---
+++ ruby/args2.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+def hoge2(arg1, arg2 = '', *args)
+  puts "arg1=#{arg1}"
+  puts "arg2=#{arg2}"
+  args.each_index{|i|
+    puts "args[#{i}]=#{args[i]}"
+  }
+end
+ 
+hoge2('hello', 'my', 'friend', '!')
+ 
+array = ['hello', 'my', 'friend', '!']
+hoge2(*array)
