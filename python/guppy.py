e0dd54a22c39d5f3294b94ee60215f79e3fb1b1a
id774 <idnanashi@gmail.com>
2012-10-15 13:47:12 +0900

    156 Add guppy.py

---
+++ python/guppy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from guppy import hpy
+h = hpy()
+N = 100000
+
+class Hoge(object):
+    def __init__(self):
+        self.x = 1
+        self.y = 1
+        self.z = 1
+        self.a = 1
+        self.b = 1
+        self.c = 1
+
+x = [Hoge() for x in range(N)]
+
+print h.heap()
