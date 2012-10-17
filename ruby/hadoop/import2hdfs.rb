e5185621d7d0100ecda331597a1d6f68c1b0aa9b
id774 <idnanashi@gmail.com>
2012-10-17 14:42:06 +0900

    162 Refactoring HDFS Importer

--- ruby/hadoop/import2hdfs.rb
+++ ruby/hadoop/import2hdfs.rb
@@ -4,38 +4,42 @@ require 'zipruby'
 require 'tempfile'
 require 'sysadmin'
 
-def import2hdfs(src, dst)
-  puts "Importing #{src} #{dst}"
-  `hadoop fs -put #{src} #{dst}`
-  puts "Done"
-end
+class ImportHDFS
+  def filelist
+    Dir.filelist(ARGV[0], true).each {|f|
+      if f =~ /\.zip\Z/
+        zip2hdfs(f)
+      else
+        import2hdfs(f, File.join(ARGV[1], f))
+      end
+    }
+  end
 
-def unzip(src)
-  Zip::Archive.open(src) do |a|
-    a.each do |f|
-      puts "Extracting #{f.name}"
-      tempfile = Tempfile::new(f.name)
-      tempfile.print(f.read)
-      import2hdfs(tempfile.path, File.join(ARGV[1], f.name))
-      tempfile.close
+  def import2hdfs(src, dst)
+    puts "Importing #{src} #{dst}"
+    `hadoop fs -put #{src} #{dst}`
+    puts "Done"
+  end
+
+  def zip2hdfs(src)
+    Zip::Archive.open(src) do |a|
+      a.each do |f|
+        puts "Extracting #{f.name}"
+        tempfile = Tempfile::new(f.name)
+        tempfile.print(f.read)
+        import2hdfs(tempfile.path, File.join(ARGV[1], File.dirname(src), f.name))
+        tempfile.close
+      end
     end
   end
 end
 
-def run
-  Dir.filelist(ARGV[0], true).each {|f|
-    if f =~ /\.zip\Z/
-      unzip(f)
-    else
-      import2hdfs(f, File.join(ARGV[1], f))
-    end
-  }
-end
-
 if __FILE__ == $0
   unless ARGV[1].nil?
-    run
+    i = ImportHDFS.new
+    i.filelist
   else
     puts "Syntax: import2hdfs zipdir hadoopdir"
+    puts "(ex.) $ ruby import2hdfs.rb data test"
   end
 end
