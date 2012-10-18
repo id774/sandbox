91b0ba8f4c5185401a4d8776e35d2ee43b9efbf9
id774 <idnanashi@gmail.com>
2012-10-18 15:57:48 +0900

    167 Add yield.py

---
+++ python/yield.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def yieldtest():
+    for i in range(10):
+        yield 10 + i
+
+if __name__=='__main__':
+    for j in yieldtest():
+        print j
+
