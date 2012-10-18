a0cc4f19784756a124cc7f6bb40bcfe3b8fe8484
id774 <idnanashi@gmail.com>
2012-10-18 10:08:13 +0900

    166 Chi-square stats example

--- python/math/chi2.py
+++ python/math/chi2.py
@@ -24,3 +24,13 @@ def chi2(count):
             v += a * a / F
     return v
 
+if __name__ == '__main__':
+    m = 3
+    n = 5
+    array = [[1 for j in range(m)] for i in range(n)]
+    array[1][1] = 2
+    array[2][0] = 4
+    array[3][1] = 3
+    print(array)
+    print(chi2(array))
+
