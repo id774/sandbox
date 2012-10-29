e3173871c4484022c27ebebe0efff454dcfed8b0
id774 <idnanashi@gmail.com>
2012-10-29 10:25:29 +0900

    173 CaboCha to XML example

---
+++ python/CaboCha/cabocha_to_xml.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import CaboCha
+
+from xml.etree import ElementTree
+from xml.etree.ElementTree import XMLID
+
+def parse_xml(xml):
+    print xml
+
+    tree, id_map = XMLID(xml)
+    for key, value in sorted(id_map.items()):
+        print '%s = %s' % (key, value)
+
+if __name__ == "__main__":
+    cabocha = CaboCha.Parser('--charset=UTF8')
+    sent = u"太郎はこの本を二郎を見た女性に渡した。".encode('utf-8')
+    tree = cabocha.parse(sent)
+    xml = tree.toString(CaboCha.FORMAT_XML)
+    parse_xml(xml)
+
