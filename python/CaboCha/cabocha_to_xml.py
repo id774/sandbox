b6e1162f32963e9ed34d0285ebe21cb3fac9076c
id774 <idnanashi@gmail.com>
2012-10-29 11:07:46 +0900

    173 CaboCha to XML example

--- python/CaboCha/cabocha_to_xml.py
+++ python/CaboCha/cabocha_to_xml.py
@@ -6,13 +6,16 @@ import CaboCha
 from xml.etree import ElementTree
 from xml.etree.ElementTree import XMLID
 
+def parse_dic(id, dic):
+    print id
+    print dic.keys()
+    print dic.items()
+
 def parse_xml(xml):
     print xml
     tree, id_map = XMLID(xml)
     for key, value in sorted(id_map.items()):
-        print '%s = %s' % (key, value)
-        print value.text
-        print value.attrib
+        parse_dic(key, value)
 
 if __name__ == "__main__":
     cabocha = CaboCha.Parser('--charset=UTF8')
