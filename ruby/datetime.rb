4783fe79eb69f1b17cd187242c0e0e06f61d485e
id774 <idnanashi@gmail.com>
2013-03-22 15:38:37 +0900

    203 DateTime in Ruby example

--- ruby/datetime.rb
+++ ruby/datetime.rb
@@ -1,11 +1,19 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
+# Date
 require 'date'
 datetime = Date.strptime("2012-10-15-11:10:15", "%Y-%m-%d-%H:%M:%S")
 p datetime.strftime("%Y-%m-%d %H:%M:%S")
 p datetime.to_s
 
+datetime += 1
+p datetime.to_s
+
+# Time
 datetime = Time.local(2012, 10, 15, 11, 10, 15)
 p datetime.strftime("%Y-%m-%d %H:%M:%S")
+p datetime.to_s
+
+datetime += 1
 p datetime.to_s
