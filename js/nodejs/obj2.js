7a28b96e9fea4274366656a5e12d81218c9e94b5
id774 <idnanashi@gmail.com>
2013-07-03 11:19:45 +0900

    240 Prototype chain

---
+++ js/nodejs/obj2.js
@@ -0,0 +1,24 @@
+var sys  = require('sys');
+var http = require('http');
+
+var Person = function (name) {
+    this.name = name;
+}
+
+Person.prototype.sayHello = function() {
+    return ('Hello ' + this.name);
+}
+
+var person = new Person('Nicole');
+
+var server = http.createServer(
+    function (request, response) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write(person.sayHello() + '\n');
+        person.name = 'hoge';
+        response.write(person.sayHello() + '\n');
+        response.end();
+    }
+).listen(3000);
+
+sys.log('Server running at http://127.0.0.1:3000/');
