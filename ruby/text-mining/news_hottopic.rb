71acd012fb673f6dead7c488114e9f3ec20ed24f
id774 <idnanashi@gmail.com>
2013-07-04 11:27:57 +0900

    241 Hotnews k-means clustering

--- ruby/text-mining/news_hottopic.rb
+++ ruby/text-mining/news_hottopic.rb
@@ -3,25 +3,30 @@
 require 'json'
 require 'date'
 require 'MeCab'
+require 'kmeans/pearson'
+require 'kmeans/hcluster'
+require 'kmeans/dendrogram'
 
 PICKUP_DATE   = (Date.today - 1).strftime("%Y%m%d")
 LOG_NAME      = "news.log.#{PICKUP_DATE}_0.log"
 WORDCOUNT     = "wordcount_#{PICKUP_DATE}.txt"
 HOT_NEWS      = "hotnews_#{PICKUP_DATE}.txt"
+IMAGEFILE     = "tree_#{PICKUP_DATE}.png"
 LOG_PATH      = "/root/.fluent/log"
 WORDCOUNT_TXT = File.expand_path(File.join(LOG_PATH, WORDCOUNT))
 INFILE        = File.expand_path(File.join(LOG_PATH, LOG_NAME))
 OUTFILE       = File.expand_path(File.join(LOG_PATH, HOT_NEWS))
+OUTIMAGE      = File.expand_path(File.join(LOG_PATH, IMAGEFILE))
+WORDS         = 150
 
 class MapReduce
   def map_reduce
     @mecab = MeCab::Tagger.new("-Ochasen")
     @text_hash = Hash.new
     @blog_hash = Hash.new{ |h,k| h[k] = Hash.new(&h.default_proc) }
-
     @word_vector = Array.new
     @hits = {}
-    @words = 150
+
     open(WORDCOUNT_TXT) do |file|
       file.each_line do |line|
         num, word, count = line.force_encoding("utf-8").strip.split("\t")
@@ -36,7 +41,10 @@ class MapReduce
           if word.length > 1
             if word =~ /[亜-腕]/
               if @text_hash.has_key?(word)
-                scoring(blog[:title], blog[:link], @text_hash[word])
+                scoring(blog[:title],
+                        blog[:link],
+                        blog[:description],
+                        @text_hash[word])
               end
             end
           end
@@ -51,23 +59,57 @@ class MapReduce
         f.write("#{i.to_s}\t#{v['score'].to_s}\t#{v['title']}\t#{k}\n")
       }
     }
+
+    entry_list = new_entrylist
+    entry_list.each {|entry|
+      i = 0
+      wordmap = new_wordmap
+      @text_hash.keys.each {|word|
+        if i < WORDS
+          @blog_hash.each {|k,v|
+            if v['title'] == entry
+              if (v['title'] + v['description']).include?(word)
+                wordmap[i] += 1
+              end
+            end
+          }
+          i += 1
+        end
+      }
+      @word_vector << wordmap
+    }
+
+    cluster = Kmeans::HCluster.new
+    hcluster = cluster.hcluster(@word_vector)
+    # p cluster.printclust(hcluster, entry_list)
+    den = Kmeans::Dendrogram.new(:imagefile => OUTIMAGE)
+    den.drawdendrogram(hcluster, entry_list)
   end
 
   private
+  def new_entrylist
+    entry_list = Array.new
+    @blog_hash.each {|k,v|
+      entry_list << v['title']
+    }
+    entry_list
+  end
+
   def new_wordmap
     wordmap = []
-    @words.times do
+    WORDS.times do
       wordmap << 0
     end
     wordmap
   end
 
-  def scoring(title, link, count)
+  def scoring(title, link, description, count)
     if @blog_hash.has_key?(link)
       @blog_hash[link]['score'] += count.to_i
     else
       @blog_hash[link]['title'] = title
       @blog_hash[link]['score'] = count.to_i
+      @blog_hash[link]['description'] = description
     end
   end
 
