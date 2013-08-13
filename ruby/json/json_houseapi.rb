7c7d68591dd51283a0c0cfd60b2a82f8a5bfd079
id774 <idnanashi@gmail.com>
2013-08-13 14:38:04 +0900

    289 Correspond example for changing API routing

--- ruby/json/json_houseapi.rb
+++ ruby/json/json_houseapi.rb
@@ -5,7 +5,7 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-uri = URI.parse('http://133.242.144.202/shibuhouse.bf_kunugi_sound')
+uri = URI.parse('http://133.242.144.202/shibuhouse/bf_kunugi_sound?limit=10')
 json = Net::HTTP.get(uri)
 result = JSON.parse(json)
 puts result
