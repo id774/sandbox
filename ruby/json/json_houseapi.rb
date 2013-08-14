6a823ff886fe8f483fab051a470262414dbfb023
id774 <idnanashi@gmail.com>
2013-08-14 10:05:15 +0900

    290 Update house api example

--- ruby/json/json_houseapi.rb
+++ ruby/json/json_houseapi.rb
@@ -5,7 +5,16 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-uri = URI.parse('http://133.242.144.202/shibuhouse/bf_kunugi_sound?limit=10')
-json = Net::HTTP.get(uri)
-result = JSON.parse(json)
+
+puts "wifi clients"
+result = JSON.parse(Net::HTTP.get(URI.parse('http://133.242.144.202/api/shibuhouse/wificlients?limit=10')))
 puts result
+
+puts "wifi temprature"
+result = JSON.parse(Net::HTTP.get(URI.parse('http://133.242.144.202/api/shibuhouse/wifitemperature?period=day&limit=30')))
+puts result
+
+puts "kunugi sound"
+result = JSON.parse(Net::HTTP.get(URI.parse('http://133.242.144.202/api/shibuhouse/bf_kunugi/sound&limit=20')))
+puts result
+
