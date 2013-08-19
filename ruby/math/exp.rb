b58e60eca56c9f3038c09c0f725808d924a95eb7
id774 <idnanashi@gmail.com>
2013-08-19 13:52:18 +0900

    297 Exponential function and logarithm

---
+++ ruby/math/exp.rb
@@ -0,0 +1,15 @@
+#!/opt/ruby/current/bin/ruby
+# -*- coding: utf-8 -*-
+
+x = 10
+
+puts "指数関数 x=10"
+p Math.exp(x)
+
+# 対数関数
+puts "x の自然対数 (e を底とする対数)"
+p Math.log (x) # loge (x)
+
+puts "x の常用対数 (10 を底とする対数)"
+p Math.log10 (x) # log10 (x)
+
