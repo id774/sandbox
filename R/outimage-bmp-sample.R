d1ba7811c3c43d2eb7dcc1a0972f35e815776cfa
id774 <idnanashi@gmail.com>
2013-08-21 16:50:25 +0900

    308 Output R to bmp

---
+++ R/outimage-bmp-sample.R
@@ -0,0 +1,9 @@
+
+time <- c(1,3,10,12,6,3,8,4,1,5)
+test <- c(20,40,100,80,50,50,70,50,10,60)
+
+bmp(filename = "graphic.bmp", width = 480, height = 480,
+   pointsize = 12, bg = "white", res = NA)
+plot(time, test)
+dev.off()
+
