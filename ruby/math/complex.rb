3ab1fa1f14f10b8937b7452329eb0777194c3fb9
id774 <idnanashi@gmail.com>
2013-08-21 14:01:44 +0900

    303 Complex

--- ruby/math/complex.rb
+++ ruby/math/complex.rb
@@ -9,3 +9,21 @@ z = Math::sqrt(x)      #=>  Complex(0.707106781186547, 0.707106781186547)
 p x
 p y
 p z
+
+p Complex(1)
+p Complex(2,3)
+p Complex.polar(2,3)
+p Complex(0,3)
+p Complex('0.3-0.5i')
+
+p Complex('2/3+3/4i')
+p Complex('1@2')
+p 3.to_c
+p 0.3.to_c
+p '0.3-0.5i'.to_c
+p '2/3+3/4i'.to_c
+p '1@2'.to_c
+
+p Complex(1, 1) / 2
+p Complex(1, 1) / 2.0
+
