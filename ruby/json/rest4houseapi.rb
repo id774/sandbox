d94829f4547a3096df62bc59e1798e97fadf801b
id774 <idnanashi@gmail.com>
2013-08-27 08:40:13 +0900

    314 Change post url

--- ruby/json/rest4houseapi.rb
+++ ruby/json/rest4houseapi.rb
@@ -4,7 +4,7 @@ require 'rest-client'
 
 json = {"hoge" => "fuga"}
 tag = "debug.forward"
-response = RestClient.post('http://username:password@133.242.144.202/solve',
+response = RestClient.post('http://username:password@133.242.144.202/post',
   {:tag => tag, :data => json},
   {:content_type => :json, :accept => :json})
 
