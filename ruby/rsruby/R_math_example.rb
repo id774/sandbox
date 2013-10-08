ca78b4fb2b0ae665f8171a4606eca05826caa79c
id774 <idnanashi@gmail.com>
2013-10-08 13:39:57 +0900

    340 R math example on ruby

---
+++ ruby/rsruby/R_math_example.rb
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+require 'rubygems'
+require "csv"
+require 'rsruby' 
+require 'awesome_print'
+
+reader = CSV.open("Book1.csv", "r") 
+header = reader.take(1)[0]
+
+T = Hash::new
+header.each do |attr|
+  T[attr.strip] = []
+end
+reader.each do |row|
+  i = 0
+  row.each do |item|
+    T[header[i].strip].push(item.strip) 
+    i = i + 1
+  end
+end
+ 
+r = RSRuby::instance
+r.eval_R(<<-RCOMMAND)
+  seq  <- c( #{T["seq"].join(",")} )
+  date <- c( #{T["date"].join(",")} )
+  USD  <- c( #{T["USD"].join(",")} )
+  EUR  <- c( #{T["EUR"].join(",")} )
+  AUD  <- c( #{T["AUD"].join(",")} )
+  D = data.frame( cbind( seq = seq, date = date, USD = USD, EUR = EUR, AUD = AUD ) )
+  a = summary(D)
+RCOMMAND
+ap r.a
