7362a7cc1608e4be7752120cf72d44078739fe82
id774 <idnanashi@gmail.com>
2013-10-08 13:50:02 +0900

    342 R scatterplot example

---
+++ ruby/rsruby/R_scatterplot_example.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require "csv"
+require 'rsruby' 
+
+reader = CSV.open("Book1.csv", "r") 
+header = reader.take(1)[0]
+
+T = Hash::new
+header.each do |attr|
+  T[attr.strip] = []
+end
+reader.each do |row|
+  i = 0
+  row.each do |item|
+    T[header[i].strip].push(item.strip) 
+    i = i + 1
+  end
+end
+
+r = RSRuby::instance
+r.eval_R(<<-RCOMMAND)
+  X <- matrix( c( #{T["seq"].join(",")} ), 1, #{T["seq"].size} )
+  Y <- matrix( c( #{T["USD"].join(",")} ), 1, #{T["USD"].size} )
+  pdf("example.pdf")
+  plot(X, Y)
+  lines(lowess( X, Y ), col = "red")
+  lines(lowess( X, Y, f=0.2 ), col = "green")
+  dev.off()
+RCOMMAND
