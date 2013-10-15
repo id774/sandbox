d1895690b35d939549bdbc9a30411b74d1bc095b
id774 <idnanashi@gmail.com>
2013-10-15 17:45:34 +0900

    358 Append comments for screening example

--- ruby/math/screening_example.rb
+++ ruby/math/screening_example.rb
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 require 'screening'
+require 'awesome_print'
 
 data = Screening::Data.new
 data.start do |element|
@@ -13,29 +14,39 @@ data.start do |element|
   element.content = "fake content"
 end
 
+puts "Initial (Class is Screening::Data)"
+puts data.class
+ap data
+
 data.push({title: "title2", content: "content2"})
-p data # => [{:title=>"title1", :content=>"content1"}, {:title=>"fake", :content=>"fake content"}, {:title=>"title2", :content=>"content2"}]
+puts "Pushed"
+ap data # => [{:title=>"title1", :content=>"content1"}, {:title=>"fake", :content=>"fake content"}, {:title=>"title2", :content=>"content2"}]
 
 puts "if you want to classify data,"
-
+ap data
+puts "High"
 data.classify(:high, :title, lambda{|e| e == "content2"})
-p data.high  # => [{:title=>"title2", :content=>"content2"}]
+ap data.high  # => [{:title=>"title2", :content=>"content2"}]
 
 puts "if you want to omit data,"
-
+ap data
+puts "Omit"
 data.omit(:title, lambda{|e| e == "fake"})
-p data # => [{:title=>"title1", :content=>"content1"}, {:title=>"title2", :content=>"content2"}]
-p data.garbage   # => [{:title=>"fake", :content=>"fake content"}]
+ap data # => [{:title=>"title1", :content=>"content1"}, {:title=>"title2", :content=>"content2"}]
+puts "Garbage"
+ap data.garbage   # => [{:title=>"fake", :content=>"fake content"}]
 
 puts "if you want to change data,"
-
+ap data
+puts "Filter"
 data.filter(:title, lambda do |e|
               e.gsub(/title/, "change") || e
             end)
-p data # => [{:title=>"change1", :content=>"content1"}, {:title=>"change2", :content=>"content2"}]
+ap data # => [{:title=>"change1", :content=>"content1"}, {:title=>"change2", :content=>"content2"}]
 
 puts "if you want NOT to make attributes of data changed,"
-
+ap data
+puts "Bind"
 data.bind([:title, :content])
 begin
   data.push({test: "test"}) # => raise error
