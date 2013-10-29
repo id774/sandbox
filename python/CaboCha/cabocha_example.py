c25f716f2b1d134a7cf38e22de35c777d3ef2269
id774 <idnanashi@gmail.com>
2013-10-29 17:45:58 +0900

    376 CaboCha example with ruby and python

--- python/CaboCha/cabocha_example.py
+++ python/CaboCha/cabocha_example.py
@@ -3,18 +3,33 @@
 
 import CaboCha
 cabocha = CaboCha.Parser('--charset=UTF8')
+#sent = "太郎はこの本を二郎を見た女性に渡した。".encode('utf-8')
 sent = u"太郎はこの本を二郎を見た女性に渡した。".encode('utf-8')
 
-print "print sent"
-print sent
+print("print sent")
+print(sent)
 
-print "print cabocha.parseToString(sent)"
-print cabocha.parseToString(sent)
+print("print cabocha.parseToString(sent)")
+print(cabocha.parseToString(sent))
 
 tree = cabocha.parse(sent)
 
-print "print tree.toString(CaboCha.FORMAT_LATTICE)"
-print tree.toString(CaboCha.FORMAT_LATTICE)
+print("print tree.toString(CaboCha.FORMAT_LATTICE)")
+print(tree.toString(CaboCha.FORMAT_LATTICE))
 
-print "print tree.toString(CaboCha.FORMAT_XML)"
-print tree.toString(CaboCha.FORMAT_XML)
+print("print tree.toString(CaboCha.FORMAT_XML)")
+print(tree.toString(CaboCha.FORMAT_XML))
+
+print("print tree.toString(0)")
+print(tree.toString(0))
+print("print tree.toString(1)")
+print(tree.toString(1))
+print("print tree.toString(2)")
+print(tree.toString(2))
+print("print tree.toString(3)")
+print(tree.toString(3))
+print("print tree.toString(4)")
+print(tree.toString(4))
+print("print tree.toString(5)")
+print(tree.toString(5))
+
