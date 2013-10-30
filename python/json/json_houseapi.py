4328709406e4e0ea5105b69f595da7d1b091e369
id774 <idnanashi@gmail.com>
2013-10-31 01:53:33 +0900

    387 House API Project client example

---
+++ python/json/json_houseapi.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+
+url       = 'http://133.242.144.202/post'
+auth      = HTTPBasicAuth("your_username", "your_password")
+headers   = {'content-type': 'application/json'}
+
+json_data = {"test":"test"}
+tag       = "debug.forward"
+
+data      = json.dumps(json_data)
+param     = {'tag':tag, 'data':data}
+r = requests.post(url, params=param, headers=headers, auth=auth)
