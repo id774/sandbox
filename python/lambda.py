7d9c2a625b5bbe92973b1fee98d02540b30046fb
id774 <idnanashi@gmail.com>
2013-11-09 16:03:14 +0900

    398 Python closure

--- python/lambda.py
+++ python/lambda.py
@@ -4,9 +4,12 @@
 def f(x):
     return x ** 3
 
+def closure(x):
+   y = lambda x: x ** 3
+   return (y(x))
+
 if __name__=='__main__':
     x = 5
-    print( f(x) )
-    y = lambda x: x ** 3
-    print( y(x) )
+    print (f(x))
+    print (closure(x))
 
