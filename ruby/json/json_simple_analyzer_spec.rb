dff50acbecb5174c39f52bc29891f66ba5b1849c
id774 <idnanashi@gmail.com>
2013-11-10 04:35:12 +0900

    400 Testing for json simple analyzer

---
+++ ruby/json/json_simple_analyzer_spec.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+$:.unshift File.join(File.dirname(__FILE__))
+
+require 'rubygems'
+require 'rspec'
+require 'json_simple_analyzer'
+
+class Output
+  attr_accessor :print
+  def initialize; @print = []; end
+  def write(msg); @print.push(msg); end
+  def self.dump
+    output = self.new
+    $stdout = output
+    yield
+    $stdout = STDOUT
+    return output.print
+  end
+end
+
+describe Analyzer do
+  context 'の start メソッドにおいて' do
+    describe 'クラスに空の引数を与えると' do
+      it 'json.txt の JSON が配列が返る' do
+        analyzer = Analyzer.new([])
+        result = Output.dump do
+          analyzer.start
+        end
+        result.length.should be_eql 46
+        result.class.should be_eql Array
+        result[44].should be_eql "23\t状況\t1"
+        result[45].should be_eql "\n"
+      end
+    end
+  end
+end
