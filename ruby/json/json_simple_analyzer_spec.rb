806baf6e6927e26d3f4f9dc4c71afc354c715412
id774 <idnanashi@gmail.com>
2013-11-11 09:54:36 +0900

    406 Fix indent

--- ruby/json/json_simple_analyzer_spec.rb
+++ ruby/json/json_simple_analyzer_spec.rb
@@ -11,6 +11,7 @@ class Output
   attr_accessor :print
   def initialize; @print = []; end
   def write(msg); @print.push(msg); end
+
   def self.dump(separator = $/)
     output = self.new
     $stdout = output
@@ -28,7 +29,31 @@ describe Analyzer do
       it 'json.txt の JSON が配列が返る' do
         analyzer = Analyzer.new([])
         result = Output.dump { analyzer.start }
-        expected = ["1\t実施\t6\n", "2\t藤原\t4\n", "3\t案件\t4\n", "4\t目的\t4\n", "5\t挨拶\t4\n", "6\t者\t2\n", "7\t数量\t2\n", "8\t増\t2\n", "9\t合わせ\t2\n", "10\t顧客\t2\n", "11\t責任\t2\n", "12\t今後\t2\n", "13\t依頼\t2\n", "14\t等\t2\n", "15\t状況\t2\n", "16\t着手\t2\n", "17\t実績\t2\n", "18\t活動\t2\n", "19\t期初\t2\n", "20\tその他\t2\n", "21\t該当\t2\n", "22\t注文\t2\n", "23\t担当\t1\n"]
+        expected = [
+          "1\t実施\t6\n",
+          "2\t藤原\t4\n",
+          "3\t案件\t4\n",
+          "4\t目的\t4\n",
+          "5\t挨拶\t4\n",
+          "6\t者\t2\n",
+          "7\t数量\t2\n",
+          "8\t増\t2\n",
+          "9\t合わせ\t2\n",
+          "10\t顧客\t2\n",
+          "11\t責任\t2\n",
+          "12\t今後\t2\n",
+          "13\t依頼\t2\n",
+          "14\t等\t2\n",
+          "15\t状況\t2\n",
+          "16\t着手\t2\n",
+          "17\t実績\t2\n",
+          "18\t活動\t2\n",
+          "19\t期初\t2\n",
+          "20\tその他\t2\n",
+          "21\t該当\t2\n",
+          "22\t注文\t2\n",
+          "23\t担当\t1\n"
+        ]
         result.length.should be_eql 23
         result.class.should be_eql Array
         result.should be_eql expected
@@ -37,10 +62,34 @@ describe Analyzer do
 
     describe 'クラスにファイル名を引数として与えると' do
       it 'ファイル内容の JSON が配列が返る' do
-        analyzer = Analyzer.new(['json2.txt'])
+        analyzer = Analyzer.new( ['json2.txt'] )
         result = Output.dump { analyzer.start }
         result.length.should be_eql 23
-        expected = ["1\t実施\t6\n", "2\t藤原\t4\n", "3\t案件\t4\n", "4\t目的\t4\n", "5\t挨拶\t4\n", "6\t者\t2\n", "7\t数量\t2\n", "8\t増\t2\n", "9\t合わせ\t2\n", "10\t顧客\t2\n", "11\t責任\t2\n", "12\t今後\t2\n", "13\t依頼\t2\n", "14\t等\t2\n", "15\tふが\t2\n", "16\t着手\t2\n", "17\t実績\t2\n", "18\t活動\t2\n", "19\t期初\t2\n", "20\tその他\t2\n", "21\t該当\t2\n", "22\t注文\t2\n", "23\tほげ\t1\n"]
+        expected = [
+          "1\t実施\t6\n",
+          "2\t藤原\t4\n",
+          "3\t案件\t4\n",
+          "4\t目的\t4\n",
+          "5\t挨拶\t4\n",
+          "6\t者\t2\n",
+          "7\t数量\t2\n",
+          "8\t増\t2\n",
+          "9\t合わせ\t2\n",
+          "10\t顧客\t2\n",
+          "11\t責任\t2\n",
+          "12\t今後\t2\n",
+          "13\t依頼\t2\n",
+          "14\t等\t2\n",
+          "15\tふが\t2\n",
+          "16\t着手\t2\n",
+          "17\t実績\t2\n",
+          "18\t活動\t2\n",
+          "19\t期初\t2\n",
+          "20\tその他\t2\n",
+          "21\t該当\t2\n",
+          "22\t注文\t2\n",
+          "23\tほげ\t1\n"
+        ]
         result.length.should be_eql 23
         result.class.should be_eql Array
         result.should be_eql expected
