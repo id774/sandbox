7cb498e82cadf1e68456ad579f9fd63377c32b44
id774 <idnanashi@gmail.com>
2013-11-12 13:13:47 +0900

    410 argv.py

---
+++ python/argv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+def main(args):
+    filename = args[1]
+    print(filename)
+
+if __name__=='__main__':
+    if len(sys.argv) > 1:
+        main(sys.argv)
+
