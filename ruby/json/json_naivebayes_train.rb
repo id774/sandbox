30b97454662a1c2c22b60417a67364e6129971b1
id774 <idnanashi@gmail.com>
2013-11-15 17:29:23 +0900

    431 Add classify method and append tests

--- ruby/json/json_naivebayes_train.rb
+++ ruby/json/json_naivebayes_train.rb
@@ -5,12 +5,13 @@ require 'naivebayes'
 
 class Analyzer
   def initialize(args)
-    @filename = args.shift || "json.txt"
+    @train_txt = args.shift || "json.txt"
+    @classify_txt = args.shift || "classify.txt"
     @classifier = NaiveBayes::Classifier.new(:model => "multinomial")
   end
 
-  def start
-    open(@filename) do |file|
+  def train
+    open(@train_txt) do |file|
       file.each_line do |line|
         key, tag, json = line.force_encoding("utf-8").strip.split("\t")
         code, name = key.strip.split(",")
@@ -36,6 +37,17 @@ class Analyzer
     end
   end
 
+  def classify
+    open(@classify_txt) do |file|
+      file.each_line do |line|
+        key, tag, json = line.force_encoding("utf-8").strip.split("\t")
+        hash = JSON.parse(json)
+        result = @classifier.classify(hash)
+        output(key, tag, JSON.generate(result))
+      end
+    end
+  end
+
   private
 
   def output(key, tag, value)
@@ -46,6 +58,7 @@ end
 
 if __FILE__ == $0
   analyzer = Analyzer.new(ARGV)
-  analyzer.start
+  analyzer.train
+  analyzer.classify
 end
 
--- ruby/json/json_naivebayes_train_spec.rb
+++ ruby/json/json_naivebayes_train_spec.rb
@@ -9,11 +9,11 @@ require 'stdout'
 require 'json_naivebayes_train'
 
 describe Analyzer do
-  context 'の start メソッドにおいて' do
+  context 'の train メソッドにおいて' do
     describe 'クラスにファイル名を引数として与えると' do
       it 'クラスとその素性が出力される' do
         analyzer = Analyzer.new( ['train.txt'] )
-        result = Stdout::Output.capture { analyzer.start }
+        result = Stdout::Output.capture { analyzer.train }
         expected = [
           "1111\t価格\t{\"競合\":1,\"企業\":1,\"入札\":1,\"選定\":1,\"落選\":1}\n",
           "1111\t価格\t{\"競合企業との入札にて落選\":1,\"入札にて落選\":1,\"選定落選\":1}\n",
@@ -21,6 +21,24 @@ describe Analyzer do
           "2222\t要員不在\t{\"要員不在につき辞退\":1,\"辞退\":1}\n"
         ]
         result.length.should eql 4
+        result.class.should eql Array
+        result.should eq expected
+      end
+    end
+  end
+
+  context 'の classify メソッドにおいて' do
+    describe 'クラスに教師データと分類対象のファイルを引数として与えると' do
+      it 'クラスとその数値が出力される' do
+        analyzer = Analyzer.new( ['train.txt','json.txt'] )
+        Stdout::Output.capture { analyzer.train }
+        result = Stdout::Output.capture { analyzer.classify }
+        expected = [
+          "1101\tああああ\t{\"価格\":0.4419727781201878,\"要員不在\":0.5580272218798122}\n",
+          "2202\tいいい\t{\"価格\":0.4419727781201878,\"要員不在\":0.5580272218798122}\n",
+          "3303\tううううう\t{\"価格\":0.4419727781201878,\"要員不在\":0.5580272218798122}\n"
+        ]
+        result.length.should eql 3
         result.class.should eql Array
         result.should eq expected
       end
