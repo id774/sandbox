903c01d3c724891d39475377c90a4b9b13c9acdd
id774 <idnanashi@gmail.com>
2013-11-20 15:44:15 +0900

    441 Summary of action type

--- ruby/json/json_summary_report.rb
+++ ruby/json/json_summary_report.rb
@@ -6,14 +6,12 @@ require 'awesome_print'
 class Analyzer
   def initialize(args)
     @filename = args.shift || "json.txt"
-    @project_success_count = 0
-    @project_failure_count = 0
-    @activity_success_count = 0
-    @activity_failure_count = 0
-    @activity_success_wordhash = {}
-    @activity_failure_wordhash = {}
+    @project_result = {}
+    @activity_action = {}
     @action_success = {}
     @action_failure = {}
+    @action_refusal = {}
+    @action_unknown = {}
   end
 
   def start
@@ -25,26 +23,39 @@ class Analyzer
         action, result = tag.strip.split(",")
         hash = JSON.parse(json)
 
-        @project_success_count += 1 if result == "受注" and type == "_project"
-        @project_failure_count += 1 if result == "失注" and type == "_project"
-        @activity_success_count += 1 if result == "受注" and type == "activity"
-        @activity_failure_count += 1 if result == "失注" and type == "activity"
+        if type == "_project"
+          @project_result.has_key?(result) ? @project_result[result] += 1 : @project_result[result] = 1
+        end
+
+        if type == "activity"
+          @activity_action.has_key?(action) ? @activity_action[action] += 1 : @activity_action[action] = 1
+        end
+
         if result == "受注" and type == "activity"
           @action_success.has_key?(action) ? @action_success[action] += 1 : @action_success[action] = 1
         end
+
         if result == "失注" and type == "activity"
           @action_failure.has_key?(action) ? @action_failure[action] += 1 : @action_failure[action] = 1
+        end
+
+        if result == "辞退" and type == "activity"
+          @action_refusal.has_key?(action) ? @action_refusal[action] += 1 : @action_refusal[action] = 1
+        end
+
+        if result == "不明" and type == "activity"
+          @action_unknown.has_key?(action) ? @action_unknown[action] += 1 : @action_unknown[action] = 1
         end
 
       end
     end
 
-    output("1", "受注案件", @project_success_count)
-    output("2", "失注案件", @project_failure_count)
-    output("3", "受注案件の活動実績", @activity_success_count)
-    output("4", "失注案件の活動実績", @activity_failure_count)
-    output("5", "受注案件の活動実績の種別", JSON.generate(@action_success))
-    output("6", "失注案件の活動実績の種別", JSON.generate(@action_failure))
+    output("1", "案件総数", JSON.generate(@project_result))
+    output("2", "活動実績種別", JSON.generate(@activity_action))
+    output("3", "受注案件の種別", JSON.generate(@action_success))
+    output("4", "失注案件の種別", JSON.generate(@action_failure))
+    output("5", "辞退案件の種別", JSON.generate(@action_refusal))
+    output("6", "不明案件の種別", JSON.generate(@action_unknown))
   end
 
   private
