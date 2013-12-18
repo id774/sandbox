f8505f35dfc4eb57a6127a4029655f68a74d3c78
id774 <idnanashi@gmail.com>
2013-12-18 17:33:50 +0900

    457 Parameter tuning

--- ruby/text-mining/deploy_fluent_scripts.sh
+++ ruby/text-mining/deploy_fluent_scripts.sh
@@ -4,6 +4,9 @@ sudo cp $HOME/sandbox/ruby/text-mining/fluentd_wordcount.rb /home/fluent/.fluent
 sudo cp $HOME/sandbox/ruby/text-mining/news_hottopic.rb /home/fluent/.fluent/bin/
 sudo cp $HOME/sandbox/ruby/text-mining/mongo_fluentd_wordcount.rb /home/fluent/.fluent/bin/
 sudo cp $HOME/sandbox/ruby/text-mining/mongo_news_hottopic.rb /home/fluent/.fluent/bin/
+sudo cp $HOME/sandbox/ruby/text-mining/wordcount_exclude.txt /home/fluent/.fluent/log/
 sudo chown -R fluent:fluent /home/fluent/.fluent/bin/
+sudo chown -R fluent:fluent /home/fluent/.fluent/log/wordcount_exclude.txt
 sudo chmod 660 /home/fluent/.fluent/bin/*
+sudo chmod 660 /home/fluent/.fluent/log/wordcount_exclude.txt
 
