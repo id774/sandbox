3b0a38b874fdded12e046a57183e87222711e76e
id774 <idnanashi@gmail.com>
2013-12-25 11:43:41 +0900

    469 Python private val

--- python/private_val.py
+++ python/private_val.py
@@ -14,3 +14,14 @@ print("member1=",instance.member1)
 print("member2=",instance._member2)
 print("member3=",instance._Klass__x)
 print("member3=",instance._Klass__member3)
+
+class ABC:
+    def __init__(self):
+        self._x = 1
+        self.__y = 2
+
+abc = ABC()
+
+print "_x =", abc._x
+print "__y =", abc._ABC__y
+print dir(abc)
