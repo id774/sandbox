a795fae9502fd0784e31cfa5dd415b6f28c79e75
id774 <idnanashi@gmail.com>
2014-01-04 03:50:18 +0900

    482 EM::SpawnedProcess

---
+++ ruby/eventmachine/em_spawned_process.rb
@@ -0,0 +1,17 @@
+require 'eventmachine'
+
+EM.run do
+  s = EM.spawn do |val|
+    puts "Received #{val}"
+  end
+
+  EM.add_timer(1) do
+    s.notify "hello"
+  end
+  EM.add_periodic_timer(1) do
+    puts "Periodic"
+  end
+  EM.add_timer(3) do
+    EM.stop
+  end
+end
