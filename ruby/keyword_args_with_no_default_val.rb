6b52fb1501a2b5730dafabd9666ae8e15cf5b456
id774 <idnanashi@gmail.com>
2014-01-10 18:09:40 +0900

    497 Keyword args demo

--- ruby/keyword_args_with_no_default_val.rb
+++ ruby/keyword_args_with_no_default_val.rb
@@ -1,7 +1,20 @@
-def hello(name:)
-  puts "Hello, #{name}"
+def hello(a:, b:1, c:, d:2)
+  p a
+  p b
+  p c
+  p d
+end
+ 
+def world(a, b=1, *c, d:, e:2, f:, **g, &h)
+  p a
+  p b
+  p c
+  p d
+  p e
+  p f
+  p g
+  p h
 end
 
-hello("World")
-
-hello
+hello(a: 3, c: 4)
+world(3, b=0, 4, d: 5, f: 6, g: 7, h: 8)
