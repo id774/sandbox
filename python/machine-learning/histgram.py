f4b19d98c9e9e3b3c1479181de3532ed732302e4
id774 <idnanashi@gmail.com>
2014-02-14 15:39:13 +0900

    529 Histgram with matplotlib

---
+++ python/machine-learning/histgram.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import numpy as np
+
+sample = 1000
+mu, sigma = 170, 5
+data = np.random.normal(mu, sigma, sample)
+
+n, bins, patches = plt.hist(data, normed=1, alpha=0.75, align='mid')
+y = mlab.normpdf(bins, mu, sigma)
+l = plt.plot(bins, y, 'r-', linewidth=1)
+
+plt.title(r'$\mathrm{Histgram\ of\ Height:}\ \mu=%d,\ \sigma=%d$' % (mu, sigma))
+plt.xlabel('Height')
+plt.ylabel('Probability')
+plt.grid(True)
+
+plt.show()
+plt.savefig("image.png")
