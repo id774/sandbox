54f59856b21e8bc2337d655c70c78ce5e62b2d33
id774 <idnanashi@gmail.com>
2014-02-15 16:11:18 +0900

    535 Array with numpy

---
+++ python/numpy/array.py
@@ -0,0 +1,20 @@
+from numpy import *
+
+a = array([1,2,3])
+print(a)
+
+f = array([1, 2, 3], dtype=float)
+print(f)
+
+a = arange(0, 10, 0.1)
+print(a)
+
+a = linspace(0, 2, 20)
+print(a)
+print(len(a))
+
+data = loadtxt("data.txt")
+print(data)
+print(data[1,:])
+print(data[:,1])
+
