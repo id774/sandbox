e2788b1832b4ef6882c0c37d7c74ec9d5aa69e70
id774 <idnanashi@gmail.com>
2014-03-24 15:12:31 +0900

    582 LaTex with matplotlib

---
+++ python/matplotlib/latex3.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as mpl
+
+# Let's create a function to model and create data
+def func(x, a, x0, sigma):
+    return a*np.exp(-(x-x0)**2/(2*sigma**2))
+
+# Generating clean data
+x = np.linspace(0, 20, 200)
+y1 = func(x[np.where(x <= 10)], 1, 3, 1)
+y2 = func(x[np.where(x > 10)], -2, 15, 0.5)
+
+y = np.hstack([y1, y2])
+
+# Adding noise to the data
+yn = y + 0.2 * np.random.normal(size=len(x))
+
+# Plot out the current state of the data and model
+fig = mpl.figure()
+ax = fig.add_subplot(111)
+ax.plot(x, y, c='k', label='Function')
+ax.scatter(x, yn)
+fig.savefig('model_and_noise_multiple.png')
+
+# Executing curve_fit on noisy data
+popt, pcov = curve_fit(func, x, yn)
+
+#popt returns the best fit values for parameters of the given model (func)
+print(popt)
+
+ym = func(x, popt[0], popt[1], popt[2])
+ax.plot(x, ym, c='r', label='Best fit')
+ax.legend()
+fig.savefig('model_fit_multiple.png')
