15d5aa86bfa8ba8c0f39d510ebe239056b23ecf9
id774 <idnanashi@gmail.com>
2014-03-25 11:26:11 +0900

    585 Poisson Distribution

---
+++ python/scipy/poisson.py
@@ -0,0 +1,23 @@
+from pylab import *
+import scipy.misc
+from pylab import *
+import matplotlib.pyplot as plt
+
+
+def poisson_pmf_gen(mu):
+  e_mu = e ** mu
+  return lambda x:mu**x / (e_mu * scipy.misc.factorial(x))
+
+for mu in arange(1, 11):
+  pmf = poisson_pmf_gen(mu)
+
+  axis([0,15, 0,pmf(mu)+0.05])
+  xlabel('x')
+  ylabel('p(x)')
+  title("Poisson distribution (mu = %d)" % mu)
+
+  x = arange(0,15)
+  bar(x, pmf(x), 0.8, color="gray")
+
+  savefig("poisson_naive_%02d" % mu)
+  clf()
