42ddccf2cb827f258310421d5ca8d2efaa2dcd96
id774 <idnanashi@gmail.com>
2014-04-03 14:05:15 +0900

    606 Refactoring

--- python/scipy/chisquare.py
+++ python/scipy/chisquare.py
@@ -7,11 +7,11 @@ def chisquare_test(array):
     '''行列に対してカイ二乗検定を行なう'''
     (num_row, num_col) = sp.shape(array)
     df = (num_row-1) * (num_col-1)
-    ch2 = get_chisquare(array)
+    ch2 = _get_chisquare(array)
     pval = 1 - stats.chi2.cdf(ch2, df)
     return ch2, pval
 
-def get_chisquare(array):
+def _get_chisquare(array):
     '''カイ二乗値を計算する'''
     sum_cols = array.sum(axis=0)
     sum_rows = array.sum(axis=1)
