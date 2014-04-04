7193a3994ab4cd6a6d3d77dae155a44d221ffb83
id774 <idnanashi@gmail.com>
2014-04-04 14:39:14 +0900

    610 Show dof, ex

--- python/scipy/chi-squared_3.py
+++ python/scipy/chi-squared_3.py
@@ -3,13 +3,15 @@
 import scipy as sp
 import scipy.stats
 
-crossed = sp.array([[320, 100],
-                    [180,  85]])
+crossed = sp.array([[435, 265],
+                    [165, 135]])
 
 x2, p, dof, expected = sp.stats.chi2_contingency(crossed)
 
 print("カイ二乗値は %(x2)s" %locals() )
 print("確率は %(p)s" %locals() )
+print("自由度は %(dof)s" %locals() )
+print( expected )
 
 if p < 0.05:
     print("有意な差があります")
