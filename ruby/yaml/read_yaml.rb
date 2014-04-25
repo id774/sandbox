67d0b310d6c524d0b4351d7416568399fada1580
id774 <idnanashi@gmail.com>
2014-04-25 09:19:32 +0900

    622 Ruby YAML example

--- ruby/yaml/read_yaml.rb
+++ ruby/yaml/read_yaml.rb
@@ -4,14 +4,16 @@ require 'yaml'
 
 config = YAML.load_file('config.yml.sample')
 
-p config["auth"]["consumer_key"]
-p config["auth"]["consumer_secret"]
+auth = config["auth"]
+p auth["consumer_key"]
+p auth["consumer_secret"]
 p config["auth"]["oauth_token"]
 p config["auth"]["oauth_token_secret"]
 
+env = config["env"]
 p config["env"]["source_screen_names"]
 p config["env"]["screen_name"]
-p config["env"]["lince_last_updated"] * 60
-p config["env"]["interval"] * 60
-p config["env"]["await"] * 60
+p env["lince_last_updated"] * 60
+p env["interval"] * 60
+p env["await"] * 60
 
