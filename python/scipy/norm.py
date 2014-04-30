16138747f2aecb4239508df63b374810fd088c9f
id774 <idnanashi@gmail.com>
2014-04-30 11:24:53 +0900

    624 Add norm.py

---
+++ python/scipy/norm.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from scipy.stats import norm
+
+print( norm.mean(), norm.std(), norm.var() )
+
+r = norm.rvs(size=100)
+print( r )
+
+print( r.mean(), r.std(), r.var() )
