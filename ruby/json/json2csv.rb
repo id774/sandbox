a04c27acc13b9197e44b821bcc048ea035c4a85d
id774 <idnanashi@gmail.com>
2014-05-01 14:38:14 +0900

    625 Add json2csv.rb

---
+++ ruby/json/json2csv.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require 'csv'
+require 'json'
+
+class Converter
+  def initialize(args)
+    @filename = args.shift || "json.txt"
+  end
+
+  def start
+    open(@filename) do |file|
+      file.each_line do |line|
+        json = line.force_encoding("utf-8").strip
+        hash = JSON.parse(json)
+
+        headers = []
+        headers.concat(hash.keys)
+        output(headers)
+
+        row = []
+        row.concat(hash.values)
+        output(row)
+      end
+    end
+  end
+
+  private
+
+  def output(array)
+    puts array.join(",")
+  end
+end
+
+if __FILE__ == $0
+  converter = Converter.new(ARGV)
+  converter.start
+end
