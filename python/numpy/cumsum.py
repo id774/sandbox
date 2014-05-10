ce18fc845e9a8203c6582abe4717af52532aff36
id774 <idnanashi@gmail.com>
2014-05-10 12:03:11 +0900

    635 NumPy.cumsum() example

---
+++ python/numpy/cumsum.py
@@ -0,0 +1,25 @@
+import numpy as np
+import scipy as sp
+
+def cumsum(nparray):
+    return nparray.cumsum()
+
+a = np.array([
+      [1,2,3],
+      [4,5,6]
+    ])
+
+print( a )
+print( np.cumsum(a) )
+
+b = np.array([
+      [2,4,6],
+      [8,10,12]
+    ])
+
+print( b )
+print( np.cumsum(b) )
+
+print( np.cumsum(a,axis=0) )
+print( np.cumsum(a,axis=1) )
+
