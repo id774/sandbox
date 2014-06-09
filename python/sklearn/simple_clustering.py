62c3230eca96e90ee3d4c6fe2b4c08154b4bd63e
id774 <idnanashi@gmail.com>
2014-06-09 12:58:23 +0900

    647 Show ranking dec

--- python/sklearn/simple_clustering.py
+++ python/sklearn/simple_clustering.py
@@ -44,6 +44,12 @@ features = np.array([
 kmeans_model = KMeans(n_clusters=3, random_state=10).fit(features)
 labels = kmeans_model.labels_
 
+ranks = []
 for label, feature in zip(labels, features):
-    print(label, feature, feature.sum())
+    ranks.append([label, feature, feature.sum()])
+
+ranks.sort(key=lambda x:(-x[2]))
+
+for rank in ranks:
+    print (rank)
 
