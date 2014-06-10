ac8bfd3e62040270fd71539c8c5e4e862b1ee9eb
id774 <idnanashi@gmail.com>
2014-06-10 18:33:38 +0900

    651 Using print with locals

--- python/locals.py
+++ python/locals.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 a = "hoge"
 b = 11
 c = True
@@ -8,10 +5,10 @@ d = None
 e = ["e",999]
 f = {"f":1}
 
-print("a の値は %(a)s, b の値は %(b)s" %locals() )
+print("a is %(a)s, b is %(b)s" %locals() )
 
-print("c の値は %(c)s\n\
-d の値は %(d)s" %locals() )
+print("c is %(c)s\n\
+d is %(d)s" %locals() )
 
-print("e の値は %(e)s, b の値は %(f)s" %locals() )
+print("e is %(e)s, b is %(f)s" %locals() )
 
