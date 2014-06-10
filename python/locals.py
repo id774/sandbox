e35e6dfc03143ab8f396ae76a536f397ebd1d4e8
id774 <idnanashi@gmail.com>
2014-06-10 18:40:57 +0900

    652 Add main.py

--- python/locals.py
+++ python/locals.py
@@ -5,10 +5,10 @@ d = None
 e = ["e",999]
 f = {"f":1}
 
-print("a is %(a)s, b is %(b)s" %locals() )
+print("a is %(a)s, b is %(b)s" %locals())
 
 print("c is %(c)s\n\
-d is %(d)s" %locals() )
+d is %(d)s" %locals())
 
-print("e is %(e)s, b is %(f)s" %locals() )
+print("e is %(e)s, b is %(f)s" %locals())
 
---
+++ python/main.py
@@ -0,0 +1,23 @@
+import sys
+
+class Analyzer:
+    def __init__(self, args):
+        self.args1 = args[1]
+        self.args2 = args[2]
+
+    def main(self):
+        print(self.args1)
+        print(self.args2)
+
+if __name__ == '__main__':
+    argsmin = 2
+    version = (3,0)
+    if sys.version_info > (version):
+        if len(sys.argv) > argsmin:
+            analyzer = Analyzer(sys.argv)
+            analyzer.main()
+        else:
+            print("This program needs at least %(argsmin)s arguments" %locals())
+    else:
+        print("This program requires python > %(version)s" %locals())
+
