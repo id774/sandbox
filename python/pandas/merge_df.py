4537640d7e77f322156943da74c6ba19e3066cf7
id774 <idnanashi@gmail.com>
2014-06-13 15:59:02 +0900

    658 Merge pandas df

---
+++ python/pandas/merge_df.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import numpy as np
+import pandas as pd
+
+wifi = pd.read_json('http://house-api-project.org/api/shibuhouse/wifi/clients?limit=100house-api-project.org/api/shibuhouse/wifi/1f/clients?limit=100')
+temp = pd.read_json('http://house-api-project.org/api/shibuhouse/1f/temperature?limit=100')
+print( wifi.head(20) )
+print( temp.head(20) )
+
+merged = pd.merge(wifi, temp, left_index=True, right_index=True)
+
+print('merged')
+print( merged.head(20) )
