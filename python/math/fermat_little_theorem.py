88e3c6c7e1fe4e74a594d1a9b9c52f8796a1e6d8
id774 <idnanashi@gmail.com>
2014-06-14 23:33:17 +0900

    661 Enable range

--- python/math/fermat_little_theorem.py
+++ python/math/fermat_little_theorem.py
@@ -6,6 +6,18 @@ def is_prime(q):
     if q < 2 or q&1 == 0: return False
     return pow(2, q-1, q) == 1
 
-for x in range(int(sys.argv[1])):
-    print(x,is_prime(x))
+def main():
+    for x in range(int(sys.argv[1]),int(sys.argv[2])):
+        print(x,is_prime(x))
+
+if __name__ == '__main__':
+    argsmin = 2
+    version = (3,0)
+    if sys.version_info > (version):
+        if len(sys.argv) > argsmin:
+            main()
+        else:
+            print("This program needs at least %(argsmin)s arguments" %locals())
+    else:
+        print("This program requires python > %(version)s" %locals())
 
