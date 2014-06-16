0ef27e0817188aa11df5f61a38d909fd84f2b761
id774 <idnanashi@gmail.com>
2014-06-16 15:34:58 +0900

    664 Pig tutorial

--- pig/tutorial.sh
+++ pig/tutorial.sh
@@ -3,6 +3,6 @@
 wget -O  tutorial.tar.gz "https://cwiki.apache.org/confluence/download/attachments/27822259/pigtutorial.tar.gz?version=1&modificationDate=1311188529000"
 tar xzvf tutorial.tar.gz
 cd pigtmp
-pig -x local script1-local.pig
-cat script1-local-results.txt/part-r-00000
+
+test -n "$1" || ../tutorial-run.sh
 
