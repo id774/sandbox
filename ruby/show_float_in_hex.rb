fab7dee9e041f809849934b955f1a3d6329b3431
id774 <idnanashi@gmail.com>
2014-06-23 19:03:01 +0900

    685 Hex packing for float

--- ruby/show_float_in_hex.rb
+++ ruby/show_float_in_hex.rb
@@ -1,2 +1,4 @@
 s=[3.14].pack("f"); printf "%02X%02X%02x%02X\n", s[0].ord,s[1].ord,s[2].ord,s[3].ord
-p [3.14].pack("f").unpack("H*")
+sss = [3.14].pack("f").unpack("H*").join
+p sss
+p "%f" % [sss].pack("H*").unpack("f")
