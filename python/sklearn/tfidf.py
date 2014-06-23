fe2d56787592c67ae191640e5aa2dcb1e110ad63
id774 <idnanashi@gmail.com>
2014-06-23 16:30:21 +0900

    678 TF-IDF with sklearn

---
+++ python/sklearn/tfidf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import os
+import MeCab
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+home = os.environ['HOME']
+target_dir = os.path.join(home, 'tmp', 'doc')
+token_dict = {}
+
+def tokenize(text):
+    wakati = MeCab.Tagger("-O wakati")
+    return wakati.parse(text)
+
+for subdir, dirs, files in os.walk(target_dir):
+    for file in files:
+        file_path = os.path.join(subdir, file)
+        shakes = open(file_path, 'r')
+        text = shakes.read()
+        lowers = text.lower()
+        token_dict[file] = lowers
+
+tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
+tfs = tfidf.fit_transform(token_dict.values())
+
+print(token_dict)
+print(tfs)
+
