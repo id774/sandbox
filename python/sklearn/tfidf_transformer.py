74652f74bf878db00337540ffe2d68aca37daa5d
id774 <idnanashi@gmail.com>
2014-06-23 17:17:22 +0900

    683 TF-IDF Transformer from sklearn

---
+++ python/sklearn/tfidf_transformer.py
@@ -0,0 +1,17 @@
+from sklearn.feature_extraction.text import TfidfTransformer
+import numpy as np
+transformer = TfidfTransformer()
+transformer
+TfidfTransformer(norm='l2', smooth_idf=True, sublinear_tf=False, use_idf=True)
+
+sample = np.array([
+    [3, 0, 1],
+    [2, 0, 0],
+    [3, 0, 0],
+    [4, 0, 0],
+    [3, 2, 0],
+    [3, 0, 2]
+    ])
+
+tfidf = transformer.fit_transform(sample)
+print(tfidf.toarray())
