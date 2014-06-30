1c57240ceb9625985d605f67bd0dc71759977a97
id774 <idnanashi@gmail.com>
2014-06-30 14:01:51 +0900

    696 JavaEE 7 my first app

---
+++ java/javaee/myfirstapp/src/main/java/com/mycompany/myfirstapp/HelloWorld.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.myfirstapp;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+/**
+ * REST Web Service
+ *
+ * @author debian
+ */
+@Path("generic")
+public class HelloWorld {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of HelloWorld
+     */
+    public HelloWorld() {
+    }
+
+    /**
+     * Retrieves representation of an instance of com.mycompany.myfirstapp.HelloWorld
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("application/xml")
+    public String getXml() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of HelloWorld
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("application/xml")
+    public void putXml(String content) {
+    }
+}
