b85ca93799fe41c51279edb33850228925cd4ba5
id774 <idnanashi@gmail.com>
2014-07-07 18:19:18 +0900

    708 Return vector

--- R/sin_05.R
+++ R/sin_05.R
@@ -1,2 +1,11 @@
 sin_0.5 <- function (x) sin(x>0.5)
+
 sin_0.5(runif(10))
+
+sinv_0.5 <- function (x) {
+    temp <- function(x) if (x<0) return(2) else return(3)
+        sapply(x, temp) }
+
+sinv_0.5(runif(10)-0.5)
+
+ifelse(runif(10) - 0.5 < 0, 2, 3)
