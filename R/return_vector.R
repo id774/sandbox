2bafe1e8ebc6b91af5eefe022b56932a9bdea51b
id774 <idnanashi@gmail.com>
2014-07-08 19:09:52 +0900

    709 Call R functions

--- R/return_vector.R
+++
@@ -1,5 +0,0 @@
-create_vector <- function (x, y) {
-  return(c(x, y, x+y))
-}
-
-create_vector(3, 4)
---
+++ R/some_function.R
@@ -0,0 +1,5 @@
+some_function <- function (x, y) {
+  return(c(x, y, x+y))
+}
+
+some_function(1, 2)
