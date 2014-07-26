e12ac5402cafe59255cf36f127061bc585bc4878
id774 <idnanashi@gmail.com>
2014-07-27 07:47:13 +0900

    743 Draw image function

--- R/outimage-png-sample.R
+++ R/outimage-png-sample.R
@@ -2,7 +2,11 @@
 time <- c(1,3,10,12,6,3,8,4,1,5)
 test <- c(20,40,100,80,50,50,70,50,10,60)
 
-png("graphic.png", width = 480, height = 480, pointsize = 12, bg = "white", res = NA)
-plot(time, test)
-dev.off()
+draw <- function(image, time, test) {
+  png(image, width = 480, height = 480, pointsize = 12, bg = "white", res = NA)
+  plot(time, test)
+  dev.off()
+}
+
+draw("image.png", time, test)
 
