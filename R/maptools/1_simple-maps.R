e948e1e41587a22b34e415ad461ead1874be7f2a
id774 <idnanashi@gmail.com>
2014-07-31 17:58:16 +0900

    753 R maptools demo

--- R/maptools/1_simple-maps.R
+++ R/maptools/1_simple-maps.R
@@ -28,13 +28,13 @@ attributes(orcounty.shp)
 attributes(orcounty.shp@data)
 attr(orcounty.shp,"polygons")
 
+# Oregon county census data -- attribute data in the orcounty.shp shape file
 png("image1-1.png", width = 480, height = 480, pointsize = 12, bg = "white", res = NA)
 plotvar <- orcounty.shp@data$POP1990
 nclr <- 8
 plotclr <- brewer.pal(nclr,"BuPu")
 class <- classIntervals(plotvar, nclr, style="quantile")
 colcode <- findColours(class, plotclr)
-
 plot(orcounty.shp, xlim=c(-124.5, -115), ylim=c(42,47))
 plot(orcounty.shp, col=colcode, add=T)
 title(main="Population 1990",
@@ -43,6 +43,7 @@ legend(-117, 44, legend=names(attr(colcode, "table")),
     fill=attr(colcode, "palette"), cex=0.6, bty="n")
 dev.off()
 
+# Oregon climate station data -- data in the orstationc.csv file, basemap in orotl.shp
 png("image1-2.png", width = 480, height = 480, pointsize = 12, bg = "white", res = NA)
 plotvar <- orstationc$tann
 nclr <- 8
@@ -50,7 +51,6 @@ plotclr <- brewer.pal(nclr,"PuOr")
 plotclr <- plotclr[nclr:1] # reorder colors
 class <- classIntervals(plotvar, nclr, style="equal")
 colcode <- findColours(class, plotclr)
-
 plot(orotl.shp, xlim=c(-124.5, -115), ylim=c(42,47))
 points(orstationc$lon, orstationc$lat, pch=16, col=colcode, cex=2)
 points(orstationc$lon, orstationc$lat, cex=2)
@@ -60,6 +60,7 @@ legend(-117, 44, legend=names(attr(colcode, "table")),
     fill=attr(colcode, "palette"), cex=0.6, bty="n")
 dev.off()
 
+# Oregon climate station data -- locations and data in shape file
 png("image1-3.png", width = 480, height = 480, pointsize = 12, bg = "white", res = NA)
 plotvar <- orstations.shp@data$pann
 nclr <- 5
@@ -68,7 +69,6 @@ class <- classIntervals(plotvar, nclr, style="fixed",
 fixedBreaks=c(0,200,500,1000,2000,5000))
 colcode <- findColours(class, plotclr)
 orstations.pts <- orstations.shp@coords # get point data
-
 plot(orotl.shp, xlim=c(-124.5, -115), ylim=c(42,47))
 points(orstations.pts, pch=16, col=colcode, cex=2)
 points(orstations.pts, cex=2)
