0ec6652a65e617529a1a1a324a843de5c7724004
id774 <idnanashi@gmail.com>
2014-08-10 07:37:32 +0900

    771 prof mod

---
+++ python/numpy/prof_mod.py
@@ -0,0 +1,28 @@
+
+import sys
+from numpy.random import randn
+
+def add_and_sum(x, y):
+    added = x + y
+    summed = added.sum(axis=1)
+    return summed
+
+def call_func():
+    x = randn(1000, 1000)
+    y = randn(1000, 1000)
+    return add_and_sum(x, y)
+
+def main(args):
+    print( call_func() )
+
+if __name__ == '__main__':
+    argsmin = 0
+    version = (3,0)
+    if sys.version_info > (version):
+        if len(sys.argv) > argsmin:
+            sys.exit(main(sys.argv))
+        else:
+            print("This program needs at least %(argsmin)s arguments" %locals())
+    else:
+        print("This program requires python > %(version)s" %locals())
+
