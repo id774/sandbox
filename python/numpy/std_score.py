2493e828f7dbab9d0eae88dd2857e1d3f31a6fd6
id774 <idnanashi@gmail.com>
2014-08-18 13:14:03 +0900

    777 Z Score

--- python/numpy/std_score.py
+++ python/numpy/std_score.py
@@ -1,5 +1,5 @@
-
 import numpy as np
+from scipy import stats
 
 sugaku = np.array([61, 74, 55, 85, 68, 72, 64, 80, 82, 59])
 kokugo = np.array([79, 81, 77, 78, 83, 80, 82, 78, 80, 82])
@@ -12,9 +12,10 @@ def test(label, a):
     print("得点", a)
     print("合計", np.sum(a))
     print("平均(average)", np.average(a))
-    print("分散( variance)", np.var(a))
-    print("標準偏差(standard deviation )", np.std(a))
+    print("分散(variance)", np.var(a))
+    print("標準偏差(standard deviation)", np.std(a))
     print("偏差値(standard score)\n", std_score(a))
+    print("Z得点(z score)\n", stats.zscore(a))
 
 test("数学", sugaku)
 test("国語", kokugo)
