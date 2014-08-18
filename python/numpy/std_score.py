d558cf7499e7fdc0ff5e557492125c7c2994f906
id774 <idnanashi@gmail.com>
2014-08-18 13:04:37 +0900

    776 Standard score

---
+++ python/numpy/std_score.py
@@ -0,0 +1,20 @@
+
+import numpy as np
+
+sugaku = np.array([61, 74, 55, 85, 68, 72, 64, 80, 82, 59])
+kokugo = np.array([79, 81, 77, 78, 83, 80, 82, 78, 80, 82])
+
+def std_score(a):
+    return np.round_(50 + 10 * (a - np.average(a)) / np.std(a))
+
+def test(label, a):
+    print(label)
+    print("得点", a)
+    print("合計", np.sum(a))
+    print("平均(average)", np.average(a))
+    print("分散( variance)", np.var(a))
+    print("標準偏差(standard deviation )", np.std(a))
+    print("偏差値(standard score)\n", std_score(a))
+
+test("数学", sugaku)
+test("国語", kokugo)
