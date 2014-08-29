0a909e35e4a6c3a4ea97edd76d3372a101a954cd
id774 <idnanashi@gmail.com>
2014-08-29 12:30:01 +0900

    794 Ping monitor

--- ruby/ping-monitor.sh
+++ ruby/ping-monitor.sh
@@ -2,13 +2,13 @@
 
 # ADMIN_MAIL_ADDRESS=xxxxxx@gmail.com
 RUBY=/opt/ruby/current/bin/ruby
-JOBLOG="ping-monitor.log"
+JOBLOG="/var/log/sysadmin/ping-monitor.log"
 TARGETS="A B C"
 
 case "$ADMIN_MAIL_ADDRESS" in
   *@*)
     $RUBY ping-monitor.rb $TARGETS > $JOBLOG
-    grep "unreachable" $JOBLOG && (nkf -w $JOBLOG | mail -s "[cron][`/bin/hostname`] Ping unreachable detected" $ADMIN_MAIL_ADDRESS)
+    grep "unreachable" $JOBLOG && (nkf -w $JOBLOG | mail -s "[admin][`/bin/hostname`] Ping unreachable detected" $ADMIN_MAIL_ADDRESS)
     ;;
 esac
 
