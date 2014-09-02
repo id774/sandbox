cdd4e11d16a2def005ce1947c90bbcf80c29838d
id774 <idnanashi@gmail.com>
2014-09-02 16:11:29 +0900

    803 Refactoring

--- python/matplotlib/histogram2d.py
+++ python/matplotlib/histogram2d.py
@@ -1,14 +1,34 @@
+import sys
 import numpy as np
-import numpy.random
 import matplotlib.pyplot as plt
 
-x = np.random.randn(8873)
-y = np.random.randn(8873)
+def draw_heatmap(x, y):
+    heatmap, xedges, yedges = np.histogram2d(x, y, bins=50)
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
 
-heatmap, xedges, yedges = np.histogram2d(x, y, bins=50)
-extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+    plt.figure()
+    plt.imshow(heatmap, extent=extent)
+    plt.show()
+    plt.savefig('image.png')
 
-plt.clf()
-plt.imshow(heatmap, extent=extent)
-plt.show()
-plt.savefig('image.png')
+def main(args):
+    t = int(args[1])
+
+    rand = lambda x: np.random.randn(x)
+    x = rand(t)
+    y = rand(t)
+
+    draw_heatmap(x, y)
+    return 0
+
+if __name__ == '__main__':
+    argsmin = 1
+    version = (3, 0)
+    if sys.version_info > (version):
+        if len(sys.argv) > argsmin:
+            sys.exit(main(sys.argv))
+        else:
+            print("This program needs at least %(argsmin)s arguments" %
+                  locals())
+    else:
+        print("This program requires python > %(version)s" % locals())
