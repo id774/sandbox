73dd60b0073dddcfa50a9daa380e59187e18cc53
id774 <idnanashi@gmail.com>
2014-09-17 14:19:50 +0900

    839 Strip Tab

--- ruby/text-mining/mongo_news_hottopic.rb
+++ ruby/text-mining/mongo_news_hottopic.rb
@@ -158,8 +158,8 @@ class HotNews
       i = 0
       @blog_hash.sort_by{|k,v| -v['score']}.each {|k, v|
         i += 1
-        quoted_title = '"' + "#{v['title']}" + '"'
-        f.write("#{i.to_s}\t#{v['score'].to_s}\t#{quoted_title}\t#{k}\t#{v['category']}\n") if v['score'] >= 10
+        title = #{v['title']}.delete("\t")
+        f.write("#{i.to_s}\t#{v['score'].to_s}\t#{title}\t#{k}\t#{v['category']}\n") if v['score'] >= 10
       }
     }
   end
