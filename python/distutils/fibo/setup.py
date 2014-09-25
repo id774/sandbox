a89f4a596b3244d2077e052a1ce994c04eb0f18f
id774 <idnanashi@gmail.com>
2014-09-25 19:48:37 +0900

    854 Distutils for Fibonacci

--- python/distutils/fibo/setup.py
+++ python/distutils/fibo/setup.py
@@ -1,4 +1,9 @@
 from distutils.core import setup
+
 setup(name='fibo',
       version='1.0',
-      py_modules=['fibo'],)
+      description='Fibonacci Number',
+      author='id774',
+      author_email='774@id774.net',
+      url='http://id774.net/',
+      packages=['fibo'],)
