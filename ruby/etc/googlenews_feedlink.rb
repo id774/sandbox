a5d56da29874419235012bba131a68b97d290488
id774 <idnanashi@gmail.com>
2014-10-12 00:26:11 +0900

    884 Pickup link from googlenews feed

--- ruby/etc/googlenews_feedlink.rb
+++ ruby/etc/googlenews_feedlink.rb
@@ -1,3 +1,12 @@
+
 s = "http://news.google.com/news/url?sa=t&fd=R&ct2=us&usg=AFQjCNGhIFo1illQ6jFyVGPZtfkttFaJYQ&clid=c3a7d30bb8a4878e06b80cf16b898331&cid=52779138313507&ei=Pts3VLDAD4X7kgWO9oGIBg&url=http://www.yomiuri.co.jp/world/20141010-OYT1T50090.html"
-m = s.match(/(&url=)/)
-p m.post_match
+
+if s.index("http://news.google.com")
+  m = s.match(/(&url=)/)
+  unless m.nil?
+    new_link = m.post_match
+    s = new_link unless new_link.nil?
+  end
+end
+
+p s
