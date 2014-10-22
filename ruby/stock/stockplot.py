8ea8de92b562a8ad9410baa4f55fc8d08cc0638a
id774 <idnanashi@gmail.com>
2014-10-22 15:39:30 +0900

    901 Plot stocks with pandas

---
+++ ruby/stock/stockplot.py
@@ -0,0 +1,42 @@
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import font_manager
+
+prop = font_manager.FontProperties(
+    fname="/usr/share/fonts/truetype/fonts-japanese-gothic.ttf")
+
+class Analyzer:
+    def __init__(self, args):
+        pass
+
+    def draw(self, code):
+        plt.figure()
+        filename = 'stock_' + code
+        ext = '.csv'
+        df = pd.read_csv(filename + ext, index_col=0)
+        df.ix[:, 3].plot(label=code)
+        plt.legend(loc='best')
+        ext = '.png'
+        plt.ylabel('Stock.close')
+        plt.xlabel('Date')
+        plt.savefig(filename + ext)
+        plt.close()
+
+    def start(self):
+        stocks = pd.read_table('stocks.txt', header=None)
+        for s in stocks.values:
+            self.draw(str(s[0]))
+
+if __name__ == '__main__':
+    argsmin = 0
+    version = (3, 0)
+    if sys.version_info > (version):
+        if len(sys.argv) > argsmin:
+            analyzer = Analyzer(sys.argv)
+            analyzer.start()
+        else:
+            print("This program needs at least %(argsmin)s arguments" %
+                  locals())
+    else:
+        print("This program requires python > %(version)s" % locals())
