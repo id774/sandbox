d7a3d0b78db5a9c13b9c27ccbe1ed0f717b71e4a
id774 <idnanashi@gmail.com>
2014-10-23 13:11:37 +0900

    906 Deploy script

--- ruby/stock/deploy_stock_scripts.sh
+++ ruby/stock/deploy_stock_scripts.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 sudo cp $HOME/sandbox/ruby/stock/stocks.sh /var/stock/
-sudo cp $HOME/sandbox/ruby/stock/stocks.txt /var/stock/
 sudo cp $HOME/sandbox/ruby/stock/stockplot.py /var/stock/
-sudo cp $HOME/sandbox/ruby/stock/jpstocks.rb /var/stock/
+sudo cp $HOME/sandbox/ruby/stock/jpstock.rb /var/stock/
 sudo chown -R root:adm /var/stock
 sudo chmod -R g+r,o-rwx /var/stock
 
