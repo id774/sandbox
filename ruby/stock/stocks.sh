a91d25222af004c26f6014b5a57dcbbe45b81fcf
id774 <idnanashi@gmail.com>
2014-10-23 12:28:15 +0900

    905 Specify ruby and python path

--- ruby/stock/stocks.sh
+++ ruby/stock/stocks.sh
@@ -2,7 +2,10 @@
 
 JOBLOG=/var/log/sysadmin/stock.log
 WORK_DIR=/var/stock
+RUBY=/opt/ruby/current/bin/ruby
+PYTHON=/opt/python/current/bin/python
 
 cd $WORK_DIR
-ruby jpstock.rb -f stocks.txt -l>>$JOBLOG 2>&1
+$RUBY jpstock.rb -f stocks.txt -l>>$JOBLOG 2>&1
 #ruby jpstock.rb -f stocks.txt -d 2000-01-01 -l>>$JOBLOG 2>&1
+$PYTHON stockplot.py>>$JOBLOG 2>&1
