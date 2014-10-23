3d2b292177e8f468d7a54d5fb3db9f42e5b0e351
id774 <idnanashi@gmail.com>
2014-10-23 12:25:35 +0900

    904 Adjust legend

--- ruby/stock/stockplot.py
+++ ruby/stock/stockplot.py
@@ -15,10 +15,10 @@ class Analyzer:
         filename = 'stock_' + code
         ext = '.csv'
         df = pd.read_csv(filename + ext, index_col=0)
-        df.ix[:, 4].plot(label="low", color="k")
-        df.ix[:, 3].plot(label="high", color="r")
-        df.ix[:, 1].plot(label="start", color="g")
-        df.ix[:, 2].plot(label="close", color="b")
+        df.ix[:, 4].plot(label="Low", color="k")
+        df.ix[:, 3].plot(label="High", color="r")
+        df.ix[:, 1].plot(label="Open", color="g")
+        df.ix[:, 2].plot(label="Close", color="b")
         plt.legend(loc='best')
         ext = '.png'
         plt.ylabel('Stock of ' + code)
