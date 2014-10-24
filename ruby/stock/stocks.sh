ff9df0f29c8dad1a28028e9f869e7904ea71c6e7
id774 <idnanashi@gmail.com>
2014-10-24 13:37:38 +0900

    909 Manual graph generation

--- ruby/stock/stocks.sh
+++ ruby/stock/stocks.sh
@@ -7,5 +7,5 @@ PYTHON=/opt/python/current/bin/python
 
 cd $WORK_DIR
 $RUBY jpstock.rb -f stocks.txt -l>>$JOBLOG 2>&1
-#ruby jpstock.rb -f stocks.txt -d 2000-01-01 -l>>$JOBLOG 2>&1
-$PYTHON stockplot.py>>$JOBLOG 2>&1
+# ruby jpstock.rb -f stocks.txt -d 2000-01-01 -l>>$JOBLOG 2>&1
+# $PYTHON stockplot.py>>$JOBLOG 2>&1
