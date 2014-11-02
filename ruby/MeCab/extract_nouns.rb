607bc4d1a7658f496cf14e05ff9ae2cd10246056
id774 <idnanashi@gmail.com>
2014-11-03 04:52:24 +0900

    913 Refactoring

--- ruby/MeCab/extract_nouns.rb
+++ ruby/MeCab/extract_nouns.rb
@@ -11,25 +11,35 @@ class Extractor
   end
 
   def main
-    Dir.glob(File.join(@in_dir, "*")).each do |infile|
-      if FileTest.file?(infile)
-        p infile
-        picked_nouns = []
-        open(infile) do |file|
-          file.each_line do |line|
-            line = line.force_encoding("utf-8").strip
-            picked_nouns.concat(pickup_nouns(line))
-          end
-        end
-        File.write(
-          File.join(@out_dir, File.basename(infile)),
-          picked_nouns.join("\n")
-        )
-      end
+    Dir.glob(File.join(@in_dir, "*")).each do |filename|
+      transform_file(filename) if FileTest.file?(filename)
     end
   end
 
   private
+
+  def transform_file(filename)
+    words = read_from_file(filename)
+    write_file(filename, words)
+  end
+
+  def read_from_file(filename)
+    picked_nouns = []
+    open(filename) do |file|
+      file.each_line do |line|
+        line = line.force_encoding("utf-8").strip
+        picked_nouns.concat(pickup_nouns(line))
+      end
+    end
+    picked_nouns
+  end
+
+  def write_file(filename, words)
+    File.write(
+      File.join(@out_dir, File.basename(filename)),
+      words.join("\n")
+    )
+  end
 
   def pickup_nouns(string)
     node = @mecab.parseToNode(string)
