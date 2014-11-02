05e3f62f96c8c8ecd8adbffe27e30e07bc9c2da4
id774 <idnanashi@gmail.com>
2014-11-03 02:44:17 +0900

    911 Refactoring

--- python/sklearn/tfidf.py
+++ python/sklearn/tfidf.py
@@ -3,7 +3,7 @@
 
 import sys
 import os
-import MeCab
+#import MeCab
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 class Tfidf:
@@ -14,25 +14,29 @@ class Tfidf:
             home = os.path.expanduser('~')
             self.target_dir = os.path.join(home, 'tmp', 'doc')
 
-    def tokenize(self, text):
-        wakati = MeCab.Tagger("-O wakati")
-        return wakati.parse(text)
-
-    def main(self):
-        token_dict = {}
+    def token_dict(self):
+        dic = {}
         for subdir, dirs, files in os.walk(self.target_dir):
             for file in files:
                 file_path = os.path.join(subdir, file)
                 shakes = open(file_path, 'r')
                 text = shakes.read()
                 lowers = text.lower()
-                token_dict[file] = lowers
+                dic[file] = lowers
                 shakes.close()
+        return dic
 
+    def tokenize(self, text):
+        #wakati = MeCab.Tagger("-O wakati")
+        #return wakati.parse(text)
+        return text.rstrip().split(",")
+
+    def main(self):
+        dic = self.token_dict()
         tfidf = TfidfVectorizer(tokenizer=self.tokenize, stop_words='english')
-        tfs = tfidf.fit_transform(token_dict.values())
+        tfs = tfidf.fit_transform(dic.values())
 
-        return(token_dict, tfs)
+        return(dic, tfs)
 
 if __name__ == '__main__':
     argsmin = 0
@@ -40,10 +44,10 @@ if __name__ == '__main__':
     if sys.version_info > (version):
         if len(sys.argv) > argsmin:
             tfidf = Tfidf(sys.argv)
-            token_dict, tfs = tfidf.main()
-            print(token_dict)
-            print(tfs)
-            print(tfs.toarray())
+            dic, tfs = tfidf.main()
+            print(dic)
+            for i, name in enumerate(dic):
+                print(name, tfs[i])
         else:
             print("This program needs at least %(argsmin)s arguments" %
                   locals())
