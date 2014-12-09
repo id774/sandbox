22877697dfe4b81d9e0037921fca86bf506c2fd4
id774 <idnanashi@gmail.com>
2014-12-09 13:23:28 +0900

    961 Calc similarity

--- python/math/similarity.py
+++ python/math/similarity.py
@@ -51,7 +51,32 @@ def matchCoeff(dicX, dicY):
     setY = set(dicY.keys())
     return len(setX & setY)
 
+def simpson(v1, v2):
+    numerator = sum([c in v2 for c in v1])
+    denominator = min(len(v1), len(v2))
+    return float(numerator) / denominator if denominator != 0 else 0
+
+def cos(v1, v2):
+    numerator = sum([v1[c] * v2[c] for c in v1 if c in v2])
+    denominator = math.sqrt(
+        sum([v * v for v in v1.values()]) * sum([v * v for v in v2.values()]))
+    return float(numerator) / denominator if denominator != 0 else 0
+
+def jaccard(v1, v2):
+    numerator = sum([c in v2 for c in v1])
+    denominator = len(v1) + len(v2) - numerator
+    return float(numerator) / denominator if denominator != 0 else 0
+
+def dice(v1, v2):
+    numerator = sum([c in v2 for c in v1])
+    denominator = len(v1) + len(v2)
+    return 2 * float(numerator) / denominator if denominator != 0 else 0
+
 print('Cosine', cosineSim(A, B))
 print('Jaccard', jaccardSim(A, B))
 print('Simpson', simpsonSim(A, B))
 print('Match', matchCoeff(A, B))
+
+print('Cosine', cos(A, B))
+print('Jaccard', jaccard(A, B))
+print('Dice', dice(A, B))
