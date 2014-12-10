fb929e34fcaa185f3a3f30ff1d2457b24e963a6b
id774 <idnanashi@gmail.com>
2014-12-10 15:51:44 +0900

    967 Refactoring

--- ruby/text-mining/scored_wordcount.rb
+++ ruby/text-mining/scored_wordcount.rb
@@ -16,34 +16,25 @@ class WordCount
       transform_file(filename) if FileTest.file?(filename)
     end
 
-    @all = sort_hash_by_value_desc(@all)
-    write_file('all_words.txt', @all)
-
-    @score_map = sort_hash_by_value_desc(@score_map)
-    write_file('all_score.txt', @score_map)
+    write_all('all_words.txt', @all)
+    write_all('all_score.txt', @score_map)
   end
 
   private
-
-  def sort_hash_by_value_desc(hash)
-    hash.sort{|a, b| b[1] <=> a[1]}
-  end
-
-  def wordcount(word, filename)
-    @hits.has_key?(word) ? @hits[word] += 1 : @hits[word] = 1
-
-    @all.has_key?(word) ? @all[word] += 1 : @all[word] = 1
-
-    basename = File.basename(filename, File.extname(filename))
-    magnification = @score_table[basename].to_f
-    @score_map.has_key?(word) ? @score_map[word] += magnification : @score_map[word] = magnification
-    @score_map[word] = @score_map[word] / @all[word]
-  end
 
   def transform_file(filename)
     read_from_file(filename)
     @hits = sort_hash_by_value_desc(@hits)
     write_file(filename, @hits)
+  end
+
+  def write_all(filename, hash)
+    sorted_hash = sort_hash_by_value_desc(hash)
+    write_file(filename, sorted_hash)
+  end
+
+  def sort_hash_by_value_desc(hash)
+    hash.sort{|a, b| b[1] <=> a[1]}
   end
 
   def read_from_score_file
@@ -63,9 +54,22 @@ class WordCount
       file.each_line do |line|
         word = line.force_encoding("utf-8").strip.split(',')[0]
         wordcount(word, filename)
+        calc_score(word, filename)
       end
     end
     @hits
+  end
+
+  def wordcount(word, filename)
+    @hits.has_key?(word) ? @hits[word] += 1 : @hits[word] = 1
+    @all.has_key?(word) ? @all[word] += 1 : @all[word] = 1
+  end
+
+  def calc_score(word, filename)
+    basename = File.basename(filename, File.extname(filename))
+    magnification = @score_table[basename].to_f
+    @score_map.has_key?(word) ? @score_map[word] += magnification : @score_map[word] = magnification
+    @score_map[word] = @score_map[word] / @all[word]
   end
 
   def write_file(filename, hash)
