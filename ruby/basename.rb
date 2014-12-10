b780660ecb69874c0da0670fe9180c3b5021a24c
id774 <idnanashi@gmail.com>
2014-12-10 14:43:03 +0900

    965 Extract basename and extname

--- ruby/basename.rb
+++ ruby/basename.rb
@@ -1,9 +1,9 @@
 def extract_basename(filename)
   basename = File.basename(filename, File.extname(filename))
-  p "#{filename} => #{basename}"
+  return "#{filename} => #{basename}"
 end
 
-extract_basename("hoge.txt")
-extract_basename("fuga.tar.gz")
-extract_basename("test.csv")
-extract_basename("a.b.c.d.e")
+p extract_basename("hoge.txt")
+p extract_basename("fuga.tar.gz")
+p extract_basename("test.csv")
+p extract_basename("a.b.c.d.e")
