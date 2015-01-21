b8df8d93b0b25f0339824d709bf3caa43a70ee6b
id774 <idnanashi@gmail.com>
2015-01-21 19:43:39 +0900

    1000 Grouping by Multi Index

--- python/pandas/groupby.py
+++ python/pandas/groupby.py
@@ -50,3 +50,9 @@ print(people.groupby(len).sum())
 
 keylist = ["one", "one", "two", "two", "two"]
 print(people.groupby([len, keylist]).sum())
+
+columns = pd.MultiIndex.from_arrays([['US', 'US', 'US', 'JP', 'JP'],
+                                     [1, 3, 5, 1, 3]], names=['cty', 'tenor'])
+hier_df = pd.DataFrame(np.random.randn(4, 5), columns=columns)
+print(hier_df)
+print(hier_df.groupby(level='cty', axis=1).count())
