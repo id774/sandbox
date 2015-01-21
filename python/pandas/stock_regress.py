b3d89a149e5882673fc7c754ff8b69d4547131d5
id774 <idnanashi@gmail.com>
2015-01-21 11:31:09 +0900

    998 Regress for Stocks

---
+++ python/pandas/stock_regress.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import statsmodels.api as sm
+from matplotlib import pyplot as plt
+from matplotlib import font_manager
+
+prop = font_manager.FontProperties(
+    fname="/usr/share/fonts/truetype/fonts-japanese-gothic.ttf")
+
+df = pd.read_csv('stocks.csv',
+                 index_col=0, parse_dates=True)
+
+rets = df.pct_change().dropna()
+by_year = rets.groupby(lambda x: x.year)
+vol_corr = lambda x: x.corrwith(x['A社'])
+
+result1 = by_year.apply(vol_corr)
+
+print(result1)
+
+plt.figure()
+result1.plot()
+plt.legend(prop=prop)
+plt.xticks([2010, 2011, 2012, 2013, 2014, 2015])
+plt.xlabel('年', fontproperties=prop)
+plt.show()
+plt.savefig("image.png")
+plt.close()
+
+result2 = by_year.apply(lambda g: g['B社'].corr(g['C社']))
+
+print(result2)
+
+def regression(data, yvar, xvars):
+    Y = data[yvar]
+    X = data[xvars]
+    X['intercept'] = 1.
+    result = sm.OLS(Y, X).fit()
+    return result.params
+
+result3 = by_year.apply(regression, 'B社', ['A社'])
+
+print(result3)
