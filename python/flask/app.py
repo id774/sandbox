9c8f4b28c8f7dec691a78217fda41d7dafb40e23
id774 <idnanashi@gmail.com>
2015-01-26 17:38:25 +0900

    1002 Flask sample

--- python/flask/app.py
+++ python/flask/app.py
@@ -1,9 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
 def hello():
-    return "Hello World!"
+    return 'Hello World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
 
 if __name__ == "__main__":
     app.debug = True
