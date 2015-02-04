c595ba63d8376753dd9d266faa261003faca34f5
id774 <idnanashi@gmail.com>
2015-02-04 20:31:36 +0900

    1015 PCA

---
+++ python/sklearn/pca.py
@@ -0,0 +1,10 @@
+
+# http://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html#sklearn.decomposition.PCA
+
+import numpy as np
+from sklearn.decomposition import PCA
+
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+pca = PCA(n_components=2)
+pca.fit(X)
+print(pca.explained_variance_ratio_)
