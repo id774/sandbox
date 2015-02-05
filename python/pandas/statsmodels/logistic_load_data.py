9d57e1695ca1bd13035dfbb796fe7c8aaed680cb
id774 <idnanashi@gmail.com>
2015-02-05 18:34:35 +0900

    1018 Plot logistec regress

--- python/pandas/statsmodels/logistic_load_data.py
+++ python/pandas/statsmodels/logistic_load_data.py
@@ -6,8 +6,60 @@ import statsmodels.api as sm
 import pylab as pl
 import numpy as np
 
+
+def cartesian(arrays, out=None):
+    """
+    Generate a cartesian product of input arrays.
+
+    Parameters
+    ----------
+    arrays : list of array-like
+        1-D arrays to form the cartesian product of.
+    out : ndarray
+        Array to place the cartesian product in.
+
+    Returns
+    -------
+    out : ndarray
+        2-D array of shape (M, len(arrays)) containing cartesian products
+        formed of input arrays.
+
+    Examples
+    --------
+    >>> cartesian(([1, 2, 3], [4, 5], [6, 7]))
+    array([[1, 4, 6],
+           [1, 4, 7],
+           [1, 5, 6],
+           [1, 5, 7],
+           [2, 4, 6],
+           [2, 4, 7],
+           [2, 5, 6],
+           [2, 5, 7],
+           [3, 4, 6],
+           [3, 4, 7],
+           [3, 5, 6],
+           [3, 5, 7]])
+
+    """
+
+    arrays = [np.asarray(x) for x in arrays]
+    dtype = arrays[0].dtype
+
+    n = np.prod([x.size for x in arrays])
+    if out is None:
+        out = np.zeros([n, len(arrays)], dtype=dtype)
+
+    m = n / arrays[0].size
+    out[:, 0] = np.repeat(arrays[0], m)
+    if arrays[1:]:
+        cartesian(arrays[1:], out=out[0:m, 1:])
+        for j in range(1, arrays[0].size):
+            out[j * m:(j + 1) * m, 1:] = out[0:m, 1:]
+    return out
+
+
 # read the data in
-#df = pd.read_csv("http://www.ats.ucla.edu/stat/data/binary.csv")
+# df = pd.read_csv("http://www.ats.ucla.edu/stat/data/binary.csv")
 df = pd.read_csv("binary.csv")
 
 # take a look at the dataset
@@ -69,3 +121,86 @@ print(result.conf_int())
 # prestige_3 -2.016992 -0.663416
 # prestige_4 -2.370399 -0.732529
 # intercept  -6.224242 -1.755716
+
+# odds ratios and 95% CI
+params = result.params
+conf = result.conf_int()
+conf['OR'] = params
+conf.columns = ['2.5%', '97.5%', 'OR']
+print(np.exp(conf))
+#                   2.5%     97.5%        OR
+# gre           1.000120  1.004418  1.002267
+# gpa           1.166122  4.281877  2.234545
+# prestige_2    0.273692  0.946358  0.508931
+# prestige_3    0.133055  0.515089  0.261792
+# prestige_4    0.093443  0.480692  0.211938
+# intercept     0.001981  0.172783  0.018500
+
+# instead of generating all possible values of GRE and GPA, we're going
+# to use an evenly spaced range of 10 values from the min to the max
+gres = np.linspace(data['gre'].min(), data['gre'].max(), 10)
+print(gres)
+# array([ 220.        ,  284.44444444,  348.88888889,  413.33333333,
+#         477.77777778,  542.22222222,  606.66666667,  671.11111111,
+#         735.55555556,  800.        ])
+gpas = np.linspace(data['gpa'].min(), data['gpa'].max(), 10)
+print(gpas)
+# array([ 2.26      ,  2.45333333,  2.64666667,  2.84      ,  3.03333333,
+#         3.22666667,  3.42      ,  3.61333333,  3.80666667,  4.        ])
+
+
+# enumerate all possibilities
+combos = pd.DataFrame(cartesian([gres, gpas, [1, 2, 3, 4], [1.]]))
+# recreate the dummy variables
+combos.columns = ['gre', 'gpa', 'prestige', 'intercept']
+dummy_ranks = pd.get_dummies(combos['prestige'], prefix='prestige')
+dummy_ranks.columns = ['prestige_1', 'prestige_2', 'prestige_3', 'prestige_4']
+
+# keep only what we need for making predictions
+cols_to_keep = ['gre', 'gpa', 'prestige', 'intercept']
+combos = combos[cols_to_keep].join(dummy_ranks.ix[:, 'prestige_2':])
+
+# make predictions on the enumerated dataset
+combos['admit_pred'] = result.predict(combos[train_cols])
+
+print(combos.head())
+#    gre       gpa  prestige  intercept  prestige_2  prestige_3  prestige_4  admit_pred
+# 0  220  2.260000         1          1           0           0           0    0.157801
+# 1  220  2.260000         2          1           1           0           0    0.087056
+# 2  220  2.260000         3          1           0           1           0    0.046758
+# 3  220  2.260000         4          1           0           0           1    0.038194
+# 4  220  2.453333         1          1           0           0
+# 0    0.179574
+
+
+def isolate_and_plot(variable, image_file):
+    # isolate gre and class rank
+    grouped = pd.pivot_table(combos, values=['admit_pred'], rows=[variable, 'prestige'],
+                             aggfunc=np.mean)
+
+    # in case you're curious as to what this looks like
+    # print grouped.head()
+    #                      admit_pred
+    # gre        prestige
+    # 220.000000 1           0.282462
+    #            2           0.169987
+    #            3           0.096544
+    #            4           0.079859
+    # 284.444444 1           0.311718
+
+    # make a plot
+    colors = 'rbgyrbgy'
+    for col in combos.prestige.unique():
+        plt_data = grouped.ix[grouped.index.get_level_values(1) == col]
+        pl.plot(plt_data.index.get_level_values(0), plt_data['admit_pred'],
+                color=colors[int(col)])
+
+    pl.xlabel(variable)
+    pl.ylabel("P(admit=1)")
+    pl.legend(['1', '2', '3', '4'], loc='upper left', title='Prestige')
+    pl.title("Prob(admit=1) isolating " + variable + " and presitge")
+    pl.show()
+    pl.savefig(image_file)
+
+isolate_and_plot('gre', 'gre.png')
+isolate_and_plot('gpa', 'gpa.png')
