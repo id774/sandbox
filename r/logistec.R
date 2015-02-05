bfc7d3b5a757ffca688b540200a6673930fda43b
id774 <idnanashi@gmail.com>
2015-02-05 19:04:11 +0900

    1019 Logistec Regression with R

---
+++ r/logistec.R
@@ -0,0 +1,32 @@
+## mydata <- read.csv("http://www.ats.ucla.edu/stat/data/binary.csv")
+mydata <- read.csv("../python/pandas/statsmodels/binary.csv")
+mydata
+summary(mydata)
+sapply(mydata, sd)
+xtabs(~admit + rank, data = mydata)
+mydata$rank <- factor(mydata$rank)
+mylogit <- glm(admit ~ gre + gpa + rank, data = mydata, family = "binomial")
+summary(mylogit)
+confint(mylogit)
+confint.default(mylogit)
+newdata1 <- with(mydata, data.frame(gre = mean(gre), gpa = mean(gpa), rank = factor(1:4)))
+## view data frame
+newdata1
+newdata1$rankP <- predict(mylogit, newdata = newdata1, type = "response")
+newdata1
+newdata2 <- with(mydata, data.frame(gre = rep(seq(from = 200, to = 800, length.out = 100),
+    4), gpa = mean(gpa), rank = factor(rep(1:4, each = 100))))
+newdata3 <- cbind(newdata2, predict(mylogit, newdata = newdata2, type = "link",
+    se = TRUE))
+newdata3 <- within(newdata3, {
+    PredictedProb <- plogis(fit)
+    LL <- plogis(fit - (1.96 * se.fit))
+    UL <- plogis(fit + (1.96 * se.fit))
+})
+## view first few rows of final dataset
+head(newdata3)
+library("ggplot2")
+ggplot(newdata3, aes(x = gre, y = PredictedProb)) + geom_ribbon(aes(ymin = LL,
+    ymax = UL, fill = rank), alpha = 0.2) + geom_line(aes(colour = rank),
+    size = 1)
+ggsave("image.png")
