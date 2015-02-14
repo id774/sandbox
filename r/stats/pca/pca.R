7020d1323c3a11f433da335f4566a3ec0770e486
id774 <idnanashi@gmail.com>
2015-02-14 16:36:14 +0900

    1041 Show summary

--- r/stats/pca/pca.R
+++ r/stats/pca/pca.R
@@ -5,23 +5,31 @@ sho = read.csv("atest2014sho.csv",
 row.names(chu) = chu[,1]
 chu = chu[,2:5]
 head(chu)
+result <- prcomp(chu, scale=TRUE)
+summary(result)
+
 png("image.png", width = 480, height = 480, pointsize = 12, bg = "white", res = NA)
 par(xpd=TRUE)
-biplot(prcomp(chu, scale=TRUE))
+biplot(result)
 dev.off()
 
 row.names(sho) = sho[,1]
 sho = sho[,2:5]
+result <- prcomp(sho, scale=TRUE)
+summary(result)
+
 png("image2.png", width = 480, height = 480, pointsize = 12, bg = "white", res = NA)
 par(xpd=TRUE)
-biplot(prcomp(sho, scale=TRUE))
+biplot(result)
 dev.off()
 
 all = data.frame(sho, chu)
 names(all) = c("小国A","小国B","小算A","小算B","中国A","中国B","中数A","中数B")
 cor(all)
-r = factanal(all, factors=2, scores="regression")
+result = factanal(all, factors=2, scores="regression")
+summary(result)
+
 png("image3.png", width = 480, height = 480, pointsize = 12, bg = "white", res = NA)
 par(xpd=TRUE)
-biplot(r$scores, r$loadings)
+biplot(result$scores, result$loadings)
 dev.off()
