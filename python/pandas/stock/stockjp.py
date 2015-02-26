7ff6b9e5e55e86ebab274ae84fdc5af2d0c4438c
id774 <idnanashi@gmail.com>
2015-02-26 18:39:22 +0900

    1055 Draw charts from stock.txt

--- python/pandas/stock/stockjp.py
+++ python/pandas/stock/stockjp.py
@@ -6,40 +6,8 @@ import sys
 import pandas as pd
 import pandas.io.data as web
 import pandas.tools.plotting as plotting
+import matplotlib.pyplot as plt
 from matplotlib.dates import date2num, AutoDateFormatter, AutoDateLocator
-
-def base_url():
-    return ('http://info.finance.yahoo.co.jp/history/'
-            '?code={0}.T&{1}&{2}&tm={3}&p={4}')
-
-def get_quote_yahoojp(code, start=None, end=None, interval='d'):
-    base = base_url()
-    start, end = web._sanitize_dates(start, end)
-    start = 'sy={0}&sm={1}&sd={2}'.format(start.year, start.month, start.day)
-    end = 'ey={0}&em={1}&ed={2}'.format(end.year, end.month, end.day)
-    p = 1
-    results = []
-
-    if interval not in ['d', 'w', 'm', 'v']:
-        raise ValueError(
-            "Invalid interval: valid values are 'd', 'w', 'm' and 'v'")
-
-    while True:
-        url = base.format(code, start, end, interval, p)
-        tables = pd.read_html(url, header=0)
-        if len(tables) < 2 or len(tables[1]) == 0:
-            break
-        results.append(tables[1])
-        p += 1
-    result = pd.concat(results, ignore_index=True)
-
-    result.columns = [
-        'Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close']
-    result['Date'] = pd.to_datetime(result['Date'], format='%Y年%m月%d日')
-    result = result.set_index('Date')
-    result = result.sort_index()
-    return result
-
 
 class OhlcPlot(plotting.LinePlot):
     ohlc_cols = pd.Index(['open', 'high', 'low', 'close'])
@@ -96,32 +64,79 @@ class OhlcPlot(plotting.LinePlot):
             ax.xaxis.set_major_formatter(AutoDateFormatter(locator))
         return candles
 
+def base_url():
+    return ('http://info.finance.yahoo.co.jp/history/'
+            '?code={0}.T&{1}&{2}&tm={3}&p={4}')
 
-plotting._all_kinds.append('ohlc')
-plotting._common_kinds.append('ohlc')
-plotting._plot_klass['ohlc'] = OhlcPlot
+def get_quote_yahoojp(code, start=None, end=None, interval='d'):
+    base = base_url()
+    start, end = web._sanitize_dates(start, end)
+    start = 'sy={0}&sm={1}&sd={2}'.format(start.year, start.month, start.day)
+    end = 'ey={0}&em={1}&ed={2}'.format(end.year, end.month, end.day)
+    p = 1
+    results = []
 
-if __name__ == '__main__':
-    import matplotlib.pyplot as plt
+    if interval not in ['d', 'w', 'm', 'v']:
+        raise ValueError(
+            "Invalid interval: valid values are 'd', 'w', 'm' and 'v'")
 
-    if not len(sys.argv) == 3:
-        print("Require 2 arguments")
-        sys.exit(1)
+    while True:
+        url = base.format(code, start, end, interval, p)
+        tables = pd.read_html(url, header=0)
+        if len(tables) < 2 or len(tables[1]) == 0:
+            break
+        results.append(tables[1])
+        p += 1
+    result = pd.concat(results, ignore_index=True)
 
-    stock_n = int(sys.argv[1])
-    start = sys.argv[2]
+    result.columns = [
+        'Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close']
+    result['Date'] = pd.to_datetime(result['Date'], format='%Y年%m月%d日')
+    result = result.set_index('Date')
+    result = result.sort_index()
+    return result
+
+def read_data(stock):
+    plotting._all_kinds.append('ohlc')
+    plotting._common_kinds.append('ohlc')
+    plotting._plot_klass['ohlc'] = OhlcPlot
+
+    start = sys.argv[1]
     # start = '2014-10-01'
 
-    stock_tse = get_quote_yahoojp(stock_n, start=start)
-    stock_tse = stock_tse[-30:]
+    try:
+        stock_tse = get_quote_yahoojp(stock, start=start)
+        stock_tse = stock_tse[-90:]
 
-    stock_tse.to_csv("".join(["stockjp_", sys.argv[1], ".csv"]))
+        stock_tse.to_csv("".join(["stockjp_", str(stock), ".csv"]))
 
-    # stock_tse.plot(kind='ohlc')
-    # plt.show()
-    # plt.savefig('image.png')
+        plt.figure()
+        # stock_tse.plot(kind='ohlc')
+        # plt.show()
+        # plt.savefig('image.png')
 
-    stock_tse.asfreq('B').plot(kind='ohlc')
-    plt.subplots_adjust(bottom=0.25)
-    plt.show()
-    plt.savefig("".join(["stockjp_", sys.argv[1], ".png"]))
+        stock_tse.asfreq('B').plot(kind='ohlc')
+        plt.subplots_adjust(bottom=0.25)
+        plt.show()
+        plt.savefig("".join(["stockjp_", str(stock), ".png"]))
+        plt.close()
+
+    except ValueError:
+        print("Value Error occured in", stock)
+
+def main():
+    stocks = pd.read_csv('stocks.txt', header=None)
+    for s in stocks.values:
+        read_data(int(s[0]))
+
+if __name__ == '__main__':
+    argsmin = 1
+    version = (3, 0)
+    if sys.version_info > (version):
+        if len(sys.argv) > argsmin:
+            main()
+        else:
+            print("This program needs at least %(argsmin)s arguments" %
+                  locals())
+    else:
+        print("This program requires python > %(version)s" % locals())
