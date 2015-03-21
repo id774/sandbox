51ddd803c82f91698b8e4b8f95bded0e1af44268
id774 <idnanashi@gmail.com>
2015-03-22 05:12:55 +0900

    1104 TA-Lib with pandas

--- python/talib/with_pandas.py
+++ python/talib/with_pandas.py
@@ -7,5 +7,13 @@ import matplotlib.pyplot as plt
 # Download SP500 data with pandas
 spyidx = data.get_data_yahoo('SPY', '2013-01-01')
 analysis = pd.DataFrame(index=spyidx.index.values)
-analysis['rsi'] = ta.RSI(np.array(spyidx.Close))
+prices = np.array(spyidx.Close)
+
+analysis['rsi'] = ta.RSI(prices)
+
+slow, fast, signal = ta.MACD(prices, fastperiod=12, slowperiod=26, signalperiod=9)
+analysis['slow'] = slow
+analysis['fast'] = fast
+analysis['signal'] = signal 
+
 print(analysis)
