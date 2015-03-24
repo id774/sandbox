492f5ebe3295fea2732d2a9f381ce2e49dfe267f
id774 <idnanashi@gmail.com>
2015-03-24 13:40:02 +0900

    1106 TA-Lib example

---
+++ python/talib/with_pandas2.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+import pandas.io.data as data
+import pandas as pd
+import talib as ta
+import matplotlib.pyplot as plt
+
+days = 30
+filename = os.path.join(os.path.dirname(
+                        os.path.abspath(__file__)),
+                        'stock_N225.csv')
+stock_tse = pd.read_csv(filename,
+                        index_col=0, parse_dates=True)
+stock_d = stock_tse.asfreq('B')[days:]
+
+analysis = pd.DataFrame(index=stock_d['Adj Close'].dropna().index.values)
+prices = np.array(stock_d['Adj Close'].dropna())
+
+analysis['prices'] = prices
+analysis['rsi'] = ta.RSI(prices)
+
+slow, fast, signal = ta.MACD(
+    prices, fastperiod=12, slowperiod=26, signalperiod=9)
+analysis['slow'] = slow
+analysis['fast'] = fast
+analysis['signal'] = signal
+
+weekly = prices
+sma50_daily = ta.SMA(np.array(weekly), timeperiod=50)
+sma200_daily = ta.SMA(np.array(weekly), timeperiod=200)
+boll_upper_weekly, boll_middle_weekly, boll_lower_weekly = ta.BBANDS(
+    weekly, timeperiod=200, nbdevup=2, nbdevdn=2, matype=0)
+analysis['boll_upper_weekly'] = boll_upper_weekly
+analysis['boll_middle_weekly'] = boll_middle_weekly
+analysis['boll_lower_weekly'] = boll_lower_weekly
+
+print(analysis)
