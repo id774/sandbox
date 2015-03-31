f95cfc138152c33bbf855cc1ba337fc52f2164ad
id774 <idnanashi@gmail.com>
2015-03-31 15:11:11 +0900

    1108 Logger in python

--- python/logger.py
+++ python/logger.py
@@ -1,8 +1,8 @@
-from logging import getLogger, StreamHandler, DEBUG
+from logging import getLogger, StreamHandler, INFO
 logger = getLogger(__name__)
 handler = StreamHandler()
-handler.setLevel(DEBUG)
-logger.setLevel(DEBUG)
+handler.setLevel(INFO)
+logger.setLevel(INFO)
 logger.addHandler(handler)
 
-logger.debug('hello')
+logger.info('hello')
