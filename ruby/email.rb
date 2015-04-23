b26fe84107bd90a5621da74e8ce10ca5f8c3ee90
id774 <idnanashi@gmail.com>
2015-04-23 09:51:59 +0900

    1117 Mail on Ruby

--- ruby/email.rb
+++ ruby/email.rb
@@ -1,28 +1,34 @@
 require 'mail'
 
-options = {
-  :address  => "localhost",
-  :port   => 25,
-  :authentication => 'plain',
-  :enable_starttls_auto => true
-}
+def sendmail
+  options = {
+    :address  => "localhost",
+    :port   => 25,
+    :authentication => 'plain',
+    :enable_starttls_auto => true
+  }
 
-Mail.defaults do
-  delivery_method :smtp, options
+  Mail.defaults do
+    delivery_method :smtp, options
+  end
+
+  filename = "email.rb"
+  dir = File.expand_path(File.dirname(__FILE__))
+  path = File.join(dir, filename)
+
+  mail = Mail.new do
+    from     "774@id774.net"
+    to       "774@id774.net"
+    subject  "メール送信のテスト"
+    body     "テストメールです"
+   add_file :filename => filename, :content => File.read(path)
+  end
+
+  mail.charset = 'utf-8'
+  mail.delivery_method :sendmail
+  mail.deliver!
 end
 
-filename = "email.rb"
-dir = File.expand_path(File.dirname(__FILE__))
-path = File.join(dir, filename)
-
-mail = Mail.new do
-  from     "774@id774.net"
-  to       "774@id774.net"
-  subject  "メール送信のテスト"
-  body     "テストメールです"
-  add_file :filename => filename, :content => File.read(path)
+if __FILE__ == $0
+  sendmail
 end
-
-mail.charset = 'utf-8'
-mail.delivery_method :sendmail
-mail.deliver!
