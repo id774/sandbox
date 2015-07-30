fc11cd51eef5813e71819ca73bde046734896076
id774 <idnanashi@gmail.com>
2015-07-30 17:40:23 +0900

    1144 Predict

--- python/sklearn/clf_sample/ridge_regression.py
+++ python/sklearn/clf_sample/ridge_regression.py
@@ -24,7 +24,8 @@ s = pd.read_csv("sampledata.csv", index_col=0, parse_dates=True)
 x, y = create_features(s['Values'], range=999)
 
 from sklearn import linear_model
-clf = linear_model.Ridge()
+clf = linear_model.Ridge(alpha=7)
+# clf = linear_model.Lasso(alpha=2)
 
 # Fitting
 print("Trained")
@@ -45,3 +46,21 @@ classify(clf, arr, -30, -16)
 classify(clf, arr, -17, -3)
 classify(clf, arr, -16, -2)
 classify(clf, arr, -15, -1)
+
+t = [1.0, 1.2, 1.1, 1.3, 1.4, 1.5, 1.1, 1.2, 1.3, 1.1, 1.0, 1.1, 1.2, 1.1]
+print(t)
+print(clf.predict(t))
+
+t = [2.0, 2.2, 2.1, 2.0, 2.4, 2.5, 2.1, 2.2, 2.3, 2.4, 2.8, 2.5, 2.7, 2.9]
+print(t)
+print(clf.predict(t))
+
+t = [1.0, 1.8, 1.1, 2.0, 2.4, 2.5, 2.1, 2.2, 2.3, 2.4, 2.8, 2.5, 2.7, 2.9]
+print(t)
+print(clf.predict(t))
+
+t = [5008.1, 5012.2, 5055.3, 5140.0, 5099.0,
+     5093.0, 5071.0, 5013.4, 5055.1, 5000.0,
+     5022.0, 5013.0, 5081.1, 5088.1]
+print(t)
+print(clf.predict(t))
