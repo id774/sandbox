6159e67e1ad3f27661133885414caced83927db5
id774 <idnanashi@gmail.com>
2015-08-03 14:18:24 +0900

    1146 Expand DataFrame

---
+++ python/pandas/expand_ts.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import datetime
+
+s = pd.read_csv("../sklearn/clf_sample/sampledata.csv", index_col=0, parse_dates=True)
+period_day = s.index[-1] + datetime.timedelta(days=1)
+index = pd.date_range(start=period_day,
+                      periods=180, freq='B')
+dup_ts = pd.Series(None, index=index)
+t = pd.concat([s, dup_ts])
+
+print(len(s))
+print(len(dup_ts))
+print(len(t))
+t.to_csv('expand_ts.csv')
