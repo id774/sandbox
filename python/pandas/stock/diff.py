0b76a3299e17a1b4dc1b3d5ee2962fbbcf6aa7e8
id774 <idnanashi@gmail.com>
2015-09-14 18:37:05 +0900

    1152 Diff stock 2 days

--- python/pandas/stock/diff.py
+++ python/pandas/stock/diff.py
@@ -2,13 +2,13 @@ import sys
 import pandas as pd
 
 def main(args):
-    file1 = args[1]
-    file2 = args[2]
-    file3 = args[3]
+    infile1 = "".join(["summary_", args[1], ".csv"])
+    infile2 = "".join(["summary_", args[2], ".csv"])
+    outfile = "".join(["diff_", args[1], "-", args[2], ".csv"])
 
     sortkey = 'Code'
-    df1 = pd.read_csv(file1, sep='\t').sort(sortkey, ascending=True)
-    df2 = pd.read_csv(file2, sep='\t').sort(sortkey, ascending=True)
+    df1 = pd.read_csv(infile1, sep='\t').sort(sortkey, ascending=True)
+    df2 = pd.read_csv(infile2, sep='\t').sort(sortkey, ascending=True)
     df3 = pd.merge(df1, df2, on='Code')
     df4 = pd.DataFrame([df3.ix[:, 'Code'],
                         df3.ix[:, 'Close_x'],
@@ -19,10 +19,10 @@ def main(args):
                                                       'Trend',
                                                       'Pred',
                                                       'Close']).T
-    df4.to_csv(file3)
+    df4.to_csv(outfile)
 
 if __name__ == '__main__':
-    argsmin = 3
+    argsmin = 2
     version = (3, 0)
     if sys.version_info > (version):
         if len(sys.argv) > argsmin:
