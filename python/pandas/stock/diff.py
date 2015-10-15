411a0b5d6b0ba9fe4ff9d7282841b198a33fbf32
id774 <idnanashi@gmail.com>
2015-10-15 16:13:03 +0900

    1154 DataFrame calculation

--- python/pandas/stock/diff.py
+++ python/pandas/stock/diff.py
@@ -1,5 +1,22 @@
 import sys
+import numpy as np
 import pandas as pd
+
+def calc_pred_u(df):
+    l = len(df)
+    result = np.zeros(l, dtype=int)
+    for i in range(l):
+        if df['Prev'][i] <= df['Pred'][i] and df['Trend'][i] == 1:
+            result[i] = 1
+    return result
+
+def calc_pred_d(df):
+    l = len(df)
+    result = np.zeros(l, dtype=int)
+    for i in range(l):
+        if df['Prev'][i] > df['Pred'][i] and df['Trend'][i] == 0:
+            result[i] = 1
+    return result
 
 def main(args):
     infile1 = "".join(["summary.csv.", args[1], ".csv"])
@@ -10,16 +27,18 @@ def main(args):
     df1 = pd.read_csv(infile1, sep='\t').sort(sortkey, ascending=True)
     df2 = pd.read_csv(infile2, sep='\t').sort(sortkey, ascending=True)
     df3 = pd.merge(df1, df2, on='Code')
-    df4 = pd.DataFrame([df3.ix[:, 'Code'],
-                        df3.ix[:, 'Close_x'],
-                        df3.ix[:, 'Trend_x'],
-                        df3.ix[:, 'Pred_x'],
-                        df3.ix[:, 'Close_y']], index=['Code',
-                                                      'Prev',
-                                                      'Trend',
-                                                      'Pred',
-                                                      'Close']).T
-    df4.to_csv(outfile, index=False)
+    df = pd.DataFrame([df3.ix[:, 'Code'],
+                       df3.ix[:, 'Close_x'],
+                       df3.ix[:, 'Trend_x'],
+                       df3.ix[:, 'Pred_x'],
+                       df3.ix[:, 'Close_y']], index=['Code',
+                                                     'Prev',
+                                                     'Trend',
+                                                     'Pred',
+                                                     'Close']).T
+    df['Pred-D'] = calc_pred_d(df)
+    df['Pred-U'] = calc_pred_u(df)
+    df.to_csv(outfile, index=False)
 
 if __name__ == '__main__':
     argsmin = 2
