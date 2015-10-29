42f6f2b9df7b13b1f7d4015df11f2324731da654
id774 <idnanashi@gmail.com>
2015-10-29 17:46:33 +0900

    1160 JMA Predict

---
+++ python/sklearn/clf_sample/jma_predict.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+
+def create_features(arr, range=3285):
+    if range > 3285:
+        range = 3285
+    range = range * -1
+    train_X = []
+    train_y = []
+    for i in np.arange(range, -360):
+        s = i + 360
+        feature = arr.ix[i:s]
+        x = feature.values
+        y = arr[s]
+        # print("train_X:", x)
+        # print("train_y:", y)
+        train_X.append(x)
+        # train_y.append(y)
+        if y <= 1:
+            train_y.append(1)
+        elif y >= 2 and y <= 8:
+            train_y.append(2)
+        else:
+            train_y.append(3)
+    return np.array(train_X), np.array(train_y)
+
+df = pd.read_csv("jma_sample.csv", index_col=0, parse_dates=True)
+
+train_df = df.ix[:-365, :]
+test_df = df.ix[-365:, :]
+
+x, y = create_features(train_df['Value'], range=3285)
+
+# Save train data to CSV
+pd.DataFrame(x).to_csv('x.csv')
+pd.DataFrame(y).to_csv('y.csv')
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
+from sklearn.lda import LDA
+
+names = ["Decision Tree",
+         "Random Forest", "AdaBoost",
+         "Gaussian Naive Bayes",
+         "Multinomial Naive Bayes",
+         "Bernoulli Naive Bayes",
+         "LDA"]
+classifiers = [
+    DecisionTreeClassifier(max_depth=5),
+    RandomForestClassifier(
+        max_depth=5, n_estimators=10, max_features=1),
+    AdaBoostClassifier(),
+    GaussianNB(), MultinomialNB(), BernoulliNB(),
+    LDA()]
+dic = dict(zip(names, classifiers))
+clf = dic["Decision Tree"]
+# clf = dic["Random Forest"]
+# clf = dic["AdaBoost"]
+# clf = dic["Gaussian Naive Bayes"]
+# clf = dic["Multinomial Naive Bayes"]
+# clf = dic["Bernoulli Naive Bayes"]
+# clf = dic["LDA"]
+
+# Fitting
+clf.fit(x, y)
+
+ruler = 361
+s = ruler * -1
+e = s + 360
+samples = test_df['Value'].ix[s:e].values
+correct = test_df['Value'].ix[e]
+print(len(samples))
+print(samples)
+print(correct)
+
+# Classify
+print(clf.predict(samples))
