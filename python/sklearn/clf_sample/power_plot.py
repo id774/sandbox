5f4fa3d59beb06e4af5657c6299757adff2a7c85
id774 <idnanashi@gmail.com>
2015-11-18 17:12:38 +0900

    1167 Plot power

---
+++ python/sklearn/clf_sample/power_plot.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+import pandas as pd
+
+datafile = 'http://www.tepco.co.jp/forecast/html/images/juyo-2014.csv'
+df_power = pd.read_csv(datafile,
+                       skiprows=3,
+                       names=['date', 'time', 'actual'],
+                       encoding='Shift_JIS')
+idx_power = pd.to_datetime(df_power['date'] + ' ' + df_power['time'])
+df_power.index = idx_power
+del df_power['date']
+del df_power['time']
+df_power_daily = df_power.resample('D', how='max', kind='period')
+
+plt.figure()
+df_power_daily.plot()
+plt.savefig('image6.png')
+plt.close()
