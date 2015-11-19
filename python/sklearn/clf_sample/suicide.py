11d07058e98b8010cf3ed4eeaecfff7ad6382643
id774 <idnanashi@gmail.com>
2015-11-19 16:47:20 +0900

    1172 Total data of suicide

--- python/sklearn/clf_sample/suicide.py
+++ python/sklearn/clf_sample/suicide.py
@@ -1,10 +1,8 @@
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 import pandas as pd
-import numpy as np
 
-month = (['20140' + str(x) for x in np.arange(1, 10)]) + \
-    (['2014' + str(x) for x in np.arange(10, 13)])
+month = pd.date_range('1/1/2014', periods=12, freq='M')
 print(month)
 
 cities = ['Tokyo', 'Fukuoka', 'Sapporo']
@@ -12,7 +10,9 @@ cities = ['Tokyo', 'Fukuoka', 'Sapporo']
 total_df = pd.DataFrame([], columns=cities)
 
 for m in month:
-    df = pd.read_csv(m + '.csv', index_col=0, skiprows=7, encoding='shift_jis')
+    df = pd.read_csv(
+        m.strftime('%Y%m') + '.csv',
+        index_col=0, skiprows=7, encoding='shift_jis')
     df = df.iloc[[12, 39, 0], [0, 1]]
     df.columns = ['City', 'Num']
     total_df.ix[m, :] = df['Num'].values
