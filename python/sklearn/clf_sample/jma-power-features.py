c1d3c1f4c3cdab92f4efa875e9490998dd86a94c
id774 <idnanashi@gmail.com>
2015-11-25 17:09:18 +0900

    1181 Refactoring

--- python/sklearn/clf_sample/jma-power-features.py
+++ python/sklearn/clf_sample/jma-power-features.py
@@ -4,31 +4,38 @@ import pandas as pd
 import numpy as np
 
 # JMA
-jma_df = pd.read_csv(
-    'JMA_3city.csv', index_col=0, parse_dates=True,
-    header=None,
-    skiprows=6,
-    encoding='Shift_JIS')
-df = jma_df.iloc[:, [0, 12, 24]]
-df.columns = ['Tokyo', 'Fukuoka', 'Sapporo']
-df.index.name = 'Date'
-# print(df['Tokyo'].values)
-# print(len(df['Tokyo'].values))
-# print(df['Tokyo'].values[0])
-# print(df['Tokyo'].values[364])
+def read_jma(filename):
+    jma_df = pd.read_csv(filename,
+                         index_col=0, parse_dates=True,
+                         header=None,
+                         skiprows=6,
+                         encoding='Shift_JIS')
+    df = jma_df.iloc[:, [0, 12, 24]]
+    df.columns = ['Tokyo', 'Fukuoka', 'Sapporo']
+    df.index.name = 'Date'
+    return df
+
+df_jma = read_jma('JMA_3city_2014.csv')
+# print(df_jma['Tokyo'])
+# print(len(df_jma['Tokyo'].values))
+# print(df_jma['Tokyo'].values[0])
+# print(df_jma['Tokyo'].values[364])
 
 # Power
-datafile = 'juyo-2014.csv'
-df_power = pd.read_csv(datafile,
-                       skiprows=3,
-                       names=['date', 'time', 'Power'],
-                       encoding='Shift_JIS')
-idx_power = pd.to_datetime(df_power['date'] + ' ' + df_power['time'])
-df_power.index = idx_power
-del df_power['date']
-del df_power['time']
-df_power_daily = df_power.resample('D', how='max', kind='period')
-# print(df_power_daily['Power'].values)
+def read_power(filename):
+    df_power = pd.read_csv(filename,
+                           skiprows=3,
+                           names=['date', 'time', 'Power'],
+                           encoding='Shift_JIS')
+    idx_power = pd.to_datetime(df_power['date'] + ' ' + df_power['time'])
+    df_power.index = idx_power
+    del df_power['date']
+    del df_power['time']
+    return df_power.resample('D', how='max', kind='period')
+
+
+df_power_daily = read_power('juyo-2014.csv')
+# print(df_power_daily['Power'])
 # print(len(df_power_daily['Power'].values))
 
 def features(x, y, range=365):
@@ -39,9 +46,9 @@ def features(x, y, range=365):
         train_y.append(y[i])
     return train_X, train_y
 
-x = df['Tokyo'].values.tolist()
+x = df_jma['Tokyo'].values.tolist()
 y = df_power_daily['Power'].values.tolist()
-x, y = features(x, y)
+x, y = features(x, y, range=365)
 
 # Machine Learning
 from sklearn.tree import DecisionTreeClassifier
@@ -72,13 +79,13 @@ clf.fit(x, y)
 
 # Classify
 predicted = clf.predict(x)
-#for (a, b) in zip (predicted, y):
-#    print(a, b)
+# for (a, b) in zip (predicted, y):
+#     print(a, b)
 
 plt.figure()
 plt.plot(predicted, label="predicted",
          color="r")
-plt.plot(y, label="answer",
+plt.plot(y, label="actual",
          color="g")
 plt.legend(loc='best')
 plt.savefig('image.png')
