4abb977761ff5f58a16abf961d3a8ab9278fab26
id774 <idnanashi@gmail.com>
2015-11-29 21:14:10 +0900

    1184 Refactoring

--- python/sklearn/clf_sample/jma-power-features.py
+++ python/sklearn/clf_sample/jma-power-features.py
@@ -3,6 +3,11 @@ plt.style.use('ggplot')
 import pandas as pd
 import numpy as np
 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
+from sklearn.lda import LDA
+
 # JMA
 def read_jma(filename):
     jma_df = pd.read_csv(filename,
@@ -27,7 +32,7 @@ def read_power(filename):
     del df_power['time']
     return df_power.resample('D', how='max', kind='period')
 
-def features(x, y, range=365):
+def create_features(x, y, range=365):
     train_X = []
     train_y = []
     for i in np.arange(0, range):
@@ -35,11 +40,12 @@ def features(x, y, range=365):
         train_y.append(y[i])
     return train_X, train_y
 
-# Machine Learning
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
-from sklearn.lda import LDA
+def read_feature(x, y):
+    df_power_daily = read_power(x)
+    df_jma = read_jma(y)
+    x = df_jma['Tokyo'].values.tolist()
+    y = df_power_daily['Power'].values.tolist()
+    return create_features(x, y, range=365)
 
 names = ["Decision Tree",
          "Random Forest", "AdaBoost",
@@ -58,31 +64,17 @@ dic = dict(zip(names, classifiers))
 clf = dic["Gaussian Naive Bayes"]
 
 # Fitting
-df_power_daily = read_power('juyo-2013.csv')
-df_jma = read_jma('JMA_3city_2013.csv')
-x = df_jma['Tokyo'].values.tolist()
-y = df_power_daily['Power'].values.tolist()
-x, y = features(x, y, range=365)
+x, y = read_feature(x='juyo-2013.csv', y='JMA_3city_2013.csv')
 clf.fit(x, y)
 
-df_power_daily = read_power('juyo-2014.csv')
-# print(df_power_daily['Power'])
-# print(len(df_power_daily['Power'].values))
+# x, y = read_feature(x='juyo-2012.csv', y='JMA_3city_2012.csv')
+# clf.fit(x, y)
 
-df_jma = read_jma('JMA_3city_2014.csv')
-# print(df_jma['Tokyo'])
-# print(len(df_jma['Tokyo'].values))
-# print(df_jma['Tokyo'].values[0])
-# print(df_jma['Tokyo'].values[364])
+# x, y = read_feature(x='juyo-2011.csv', y='JMA_3city_2011.csv')
+# clf.fit(x, y)
 
-x = df_jma['Tokyo'].values.tolist()
-y = df_power_daily['Power'].values.tolist()
-x, y = features(x, y, range=365)
-
-# Classify
+x, y = read_feature(x='juyo-2014.csv', y='JMA_3city_2014.csv')
 predicted = clf.predict(x)
-# for (a, b) in zip (predicted, y):
-#     print(a, b)
 
 plt.figure()
 plt.plot(predicted, label="predicted",
