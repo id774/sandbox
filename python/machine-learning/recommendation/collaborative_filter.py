51441fba4b7ee8a28212a703ce583e527d927d68
id774 <idnanashi@gmail.com>
2015-12-14 17:07:15 +0900

    1188 Collaborative filtering recommendation engine implementation in python

---
+++ python/machine-learning/recommendation/collaborative_filter.py
@@ -0,0 +1,9 @@
+from recommendation_data import dataset
+
+print("Lisa Rose rating on Lady in the water: {}\n".format(
+    dataset['Lisa Rose']['Lady in the Water']))
+print("Michael Phillips rating on Lady in the water: {}\n".format(
+    dataset['Michael Phillips']['Lady in the Water']))
+
+print('**************Jack Matthews ratings**************')
+print(dataset['Jack Matthews'])
