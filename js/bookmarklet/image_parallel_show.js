744d6ec936e0a83fc0c9303339b92b51c1e5f623
id774 <idnanashi@gmail.com>
2015-12-17 17:52:49 +0900

    1190 Image parralel display

---
+++ js/bookmarklet/image_parallel_show.js
@@ -0,0 +1,1 @@
+javascript:(function(){ var _d = document; var array = _d.body.innerHTML.match(/("|')([^"']+?\.)(jpg|gif|png|bmp|jpeg)("|')/ig); if(!array) return alert('Not Found'); var exist = {}; for(var i=0; i < array.length; i++) { if(!exist[array[i]]) { _d.write('<img src=' + array[i] + '>'); exist[array[i]] = 1 } } _d.close()})()
