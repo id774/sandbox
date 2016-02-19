e2b3c42de4447742dac114ec0374df35cc180444
id774 <idnanashi@gmail.com>
2016-02-19 10:50:48 +0900

    1195 Add sklearn samples

---
+++ python/sklearn/plot_pca_vs_lda.py
@@ -0,0 +1,40 @@
+print(__doc__)
+
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+iris = datasets.load_iris()
+
+X = iris.data
+y = iris.target
+target_names = iris.target_names
+
+pca = PCA(n_components=2)
+X_r = pca.fit(X).transform(X)
+
+lda = LinearDiscriminantAnalysis(n_components=2)
+X_r2 = lda.fit(X, y).transform(X)
+
+# Percentage of variance explained for each components
+print('explained variance ratio (first two components): %s'
+      % str(pca.explained_variance_ratio_))
+
+plt.figure()
+for c, i, target_name in zip("rgb", [0, 1, 2], target_names):
+    plt.scatter(X_r[y == i, 0], X_r[y == i, 1], c=c, label=target_name)
+plt.legend()
+plt.title('PCA of IRIS dataset')
+plt.show()
+plt.savefig('image.png')
+
+plt.figure()
+for c, i, target_name in zip("rgb", [0, 1, 2], target_names):
+    plt.scatter(X_r2[y == i, 0], X_r2[y == i, 1], c=c, label=target_name)
+plt.legend()
+plt.title('LDA of IRIS dataset')
+
+plt.show()
+plt.savefig('image2.png')
