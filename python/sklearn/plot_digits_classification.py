640647d842db11f9f20caf4cd2802afbc9c401cb
id774 <idnanashi@gmail.com>
2016-02-24 16:12:49 +0900

    1202 Increase Digits

--- python/sklearn/plot_digits_classification.py
+++ python/sklearn/plot_digits_classification.py
@@ -10,6 +10,7 @@ This example is commented in the
 :ref:`tutorial section of the user manual <introduction>`.
 
 """
+
 print(__doc__)
 
 # Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
@@ -23,6 +24,10 @@ from sklearn import datasets, svm, metrics
 
 # The digits dataset
 digits = datasets.load_digits()
+# for i in digits.images:
+#     print(i)
+# for t in digits.target:
+#     print(t)
 
 # The data that we are interested in is made of 8x8 images of digits, let's
 # have a look at the first 3 images, stored in the `images` attribute of the
@@ -30,9 +35,12 @@ digits = datasets.load_digits()
 # pylab.imread.  Note that each image must have the same size. For these
 # images, we know which digit they represent: it is given in the 'target' of
 # the dataset.
+
+plt.figure(figsize=(12.80, 10.24))
+
 images_and_labels = list(zip(digits.images, digits.target))
-for index, (image, label) in enumerate(images_and_labels[:4]):
-    plt.subplot(2, 4, index + 1)
+for index, (image, label) in enumerate(images_and_labels[:5]):
+    plt.subplot(2, 5, index + 1)
     plt.axis('off')
     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
     plt.title('Training: %i' % label)
@@ -49,16 +57,19 @@ classifier = svm.SVC(gamma=0.001)
 classifier.fit(data[:n_samples / 2], digits.target[:n_samples / 2])
 
 # Now predict the value of the digit on the second half:
+print(n_samples)
 expected = digits.target[n_samples / 2:]
+print("exceted", expected)
 predicted = classifier.predict(data[n_samples / 2:])
+print("predicted", predicted)
 
 print("Classification report for classifier %s:\n%s\n"
       % (classifier, metrics.classification_report(expected, predicted)))
 print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
 
 images_and_predictions = list(zip(digits.images[n_samples / 2:], predicted))
-for index, (image, prediction) in enumerate(images_and_predictions[:4]):
-    plt.subplot(2, 4, index + 5)
+for index, (image, prediction) in enumerate(images_and_predictions[:5]):
+    plt.subplot(2, 5, index + 6)
     plt.axis('off')
     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
     plt.title('Prediction: %i' % prediction)
