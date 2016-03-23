b7360513edf7c697300dbda3625a22fdc065a152
id774 <idnanashi@gmail.com>
2016-03-23 09:36:58 +0900

    1214 Refactoring, Using MiniTest

--- ruby/regexp/http.rb
+++ ruby/regexp/http.rb
@@ -1,3 +1,6 @@
+gem 'minitest'
+
+require 'minitest/autorun'
 require 'erb'
 require 'set'
 require 'uri'
@@ -16,9 +19,8 @@ def select(arg)
     statuses = Termtter::API.twitter.user_timeline(:screen_name => user_name)
     return if statuses.empty?
     statuses[0].id
-  when %r{twitter.com/(?:\#!/)[A-Za-z0-9_]+/status(?:es)?/\d+}
-    URI.parse(arg).path.split(%{/}).last.to_i
-  when %r{twitter.com/[A-Za-z0-9_]+}
+  when %r{twitter.com/(?:\#!/)[A-Za-z0-9_]+/status(?:es)?/\d+},
+    %r{twitter.com/[A-Za-z0-9_]+}
     URI.parse(arg).path.split(%{/}).last.to_i
   when /^\/(.*)$/
     word = $1
@@ -32,6 +34,18 @@ def select(arg)
   end
 end
 
-h = "https://twitter.com/twitt/status/711901052023341060"
-a = select(h)
-p a
+class TestSelect < MiniTest::Test
+  def test_select
+    expected = 711901052023341060
+
+    h = "https://twitter.com/twitt/status/711901052023341060"
+    ans = select(h)
+    assert_equal expected, ans
+
+    h = "https://twitter.com/711901052023341060"
+    ans = select(h)
+    assert_equal expected, ans
+  end
+end
+
+MiniTest.autorun
