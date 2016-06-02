ad1700f061bb713a7c2b846e94d4d3a0c83bc851
id774 <idnanashi@gmail.com>
2016-06-02 13:29:36 +0900

    1222 Deprecation after sklearn v0.17

--- python/sklearn/deprecation_for_0.17.py
+++ python/sklearn/deprecation_for_0.17.py
@@ -6,4 +6,7 @@ Y = np.array([11, 23, 43, 170.5, 934.6])
 model = linear_model.LinearRegression()
 X = X.reshape(-1, 1)
 model.fit(X, Y)
+
+# print("Predict for number 12 {}".format(model.predict(np.array([12]).reshape(-1, 1))[0]))
+
 print("Predict for number 12 {}".format(model.predict([12])[0]))
