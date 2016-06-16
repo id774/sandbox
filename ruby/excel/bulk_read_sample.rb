acb2a0d365b40b5824437f85b549de599333d0b5
id774 <idnanashi@gmail.com>
2016-06-16 14:03:27 +0900

    1235 Excel read example

--- ruby/excel/bulk_read_sample.rb
+++ ruby/excel/bulk_read_sample.rb
@@ -8,6 +8,16 @@ sheet1 = book.worksheet 0
 
 sheet1.each_with_index {|row, i|
   if row[34].to_s.match(/【案件/)
-    puts "#{i}: #{row[4]}"
+    # puts "案件番号 #{i}: #{row[4]}"
+    puts "案件番号 #{i}: #{sheet1[i, 4].to_s.match(/^\d{4}-\d{5}/)}"
+    puts "案件名 #{i}: #{sheet1[i+6, 9].gsub(/[\r\n]/," ")}" unless sheet1[i, 4].nil?
+    puts "期間 #{i}: #{sheet1[i, 16]}"
+    puts "売上規模 #{i}: #{sheet1[i+1, 16]}"
+    puts "見込み利益率 #{i}: #{sheet1[i+3, 16]}"
+    puts "工程 #{i}: #{sheet1[i+4, 16]}"
+    puts "案件種別 #{i}: #{sheet1[i+5, 16]}"
+    puts "社員数 #{i}: #{sheet1[i, 32]}"
+    puts "BP 数 #{i}: #{sheet1[i+1, 32]}"
+    puts "PM #{i}: #{sheet1[i+3, 30].gsub(/[\r\n]/," ")}" unless sheet1[i+3, 30].nil?
   end
 }
