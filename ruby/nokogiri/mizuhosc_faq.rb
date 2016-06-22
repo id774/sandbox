d6abef2d12053cf03aa5e87d816300d5b128a242
id774 <idnanashi@gmail.com>
2016-06-22 11:33:22 +0900

    1237 Scraping with Nokogiri

---
+++ ruby/nokogiri/mizuhosc_faq.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'nkf'
+
+url = "https://faq.mizuho-sc.com/faq/show/1055"
+charset = nil
+
+html = open(url) do |f|
+  charset = f.charset
+  f.read
+end
+
+doc = Nokogiri::HTML.parse(html, nil, charset)
+
+doc.xpath('//span[@class="icoQ"]').each do |node|
+  puts "質問, #{node.text.strip}"
+end
+
+doc.xpath('//div[@class="faq_ansCont_txt clearfix"]').each do |node|
+  puts "回答, #{node.text.strip}"
+end
