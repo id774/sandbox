5cf3d1a787bd7562f88883b753331e9ba63930f6
id774 <idnanashi@gmail.com>
2016-07-27 13:30:49 +0900

    1247 Backup for PostgreSQL all data

--- sql/postgresql/pg_backup.sh
+++ sql/postgresql/pg_backup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/pg_dumpall -c > /var/lib/postgresql/backup/all.dump && gzip /var/lib/postgresql/backup/all.dump
+/usr/bin/pg_dumpall -c > /var/lib/postgresql/backup/all.dump && gzip -f /var/lib/postgresql/backup/all.dump>/dev/null 2>&1
