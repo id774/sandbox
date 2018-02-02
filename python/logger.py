a56600ea97e124f897fbc71ab91911959987b34c
id774 <idnanashi@gmail.com>
2018-02-02 14:50:46 +0900

    1263 Python logger example

--- python/logger.py
+++ python/logger.py
@@ -1,8 +1,13 @@
-from logging import getLogger, StreamHandler, INFO
+from logging import getLogger, Formatter, StreamHandler, INFO
 logger = getLogger(__name__)
 handler = StreamHandler()
 handler.setLevel(INFO)
+handler.setFormatter(Formatter(fmt='%(asctime)s %(levelname)s -- %(message)s', datefmt="%Y-%m-%dT%H:%M:%S+09:00"))
 logger.setLevel(INFO)
 logger.addHandler(handler)
+logger.propagate = False
 
-logger.info('hello')
+logger.debug('debug')
+logger.info('info')
+logger.warn('warn')
+logger.error('error')
